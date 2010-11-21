2252392a54f1bac326aab2eb06cc8137213fd4c8
objo <objo@localhost>
2010-11-21 15:35:53 +0000

    Added reportwriters

---
+++ Source/Oxyplot.Reporting/ReportWriters/IReportWriter.cs
@@ -0,0 +1,12 @@
+﻿namespace OxyPlot.Reporting
+{
+    public interface IReportWriter
+    {
+        void WriteHeader(Header h);
+        void WriteParagraph(Paragraph p);
+        void WriteTable(Table t);
+        void WriteImage(Image i);
+        void WriteDrawing(Drawing d);
+        void WritePlot(Plot plot);
+    }
+}
