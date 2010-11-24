9031f6ddb35c56d9d3dcc63df9e8de202b441961
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-11-24 07:38:13 +0000

    Added ExportDemo

--- Source/Examples/ExportDemo/Properties/AssemblyInfo.cs
+++ Source/Examples/ExportDemo/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleDemo")]
+[assembly: AssemblyTitle("ExportDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleDemo")]
+[assembly: AssemblyProduct("ExportDemo")]
 [assembly: AssemblyCopyright("Copyright © OBJO 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
