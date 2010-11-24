14f48a1965b7b2d5b5bd8ec08b7c63bbbb434328
objo <objo@localhost>
2010-11-24 14:37:12 +0000

    Added CSV example

--- Source/Examples/CsvDemo/MainWindow.xaml.cs
+++ Source/Examples/CsvDemo/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using OxyPlot.Pdf;
 
 namespace CsvDemo
 {
@@ -20,95 +12,116 @@ namespace CsvDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainViewModel vm = new MainViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = vm;
+            vm.Open("GlobalTemperatureAnomaly.csv");
+            // vm.Open("WorldPopulation.csv");
+            vm.Open("RiverFlow.csv");
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
-
-        }
-
-        private void OpenCsv_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SaveSvg_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SavePdf_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SavePng_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SaveXaml_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SaveHtmlReport_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SavePdfReport_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            var data = e.Data as DataObject;
+            if (data != null && data.ContainsFileDropList())
+            {
+                foreach (var file in data.GetFileDropList())
+                {
+                    vm.Open(file);
+                }
+            }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
-            throw new NotImplementedException();
         }
 
-        private void CopySvg_Click(object sender, RoutedEventArgs e)
+        private void OpenCsv_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void CopyBitmap_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CopyXaml_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void About_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            var dlg = new OpenFileDialog();
+            dlg.Filter = ".csv files|*.csv";
+            dlg.DefaultExt = ".csv";
+            if (dlg.ShowDialog(this).Value)
+            {
+                vm.Open(dlg.FileName);
+            }
         }
 
         private void SavePlot_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var dlg = new SaveFileDialog();
+            dlg.Filter = ".svg files|*.svg|.png files|*.png|.pdf files|*.pdf|.xaml files|*.xaml";
+            dlg.DefaultExt = ".svg";
+            if (dlg.ShowDialog(this).Value)
+            {
+                var ext = Path.GetExtension(dlg.FileName).ToLower();
+                switch (ext)
+                {
+                    case ".png":
+                        plot1.SaveBitmap(dlg.FileName);
+                        break;
+                    case ".svg":
+                        vm.Model.SaveSvg(dlg.FileName, plot1.ActualWidth, plot1.ActualHeight);
+                        break;
+                    case ".pdf":
+                        PdfPlotWriter.Save(vm.Model, dlg.FileName, plot1.ActualWidth, plot1.ActualHeight);
+                        break;
+                    case ".xaml":
+                        plot1.SaveXaml(dlg.FileName);
+                        break;
+                }
+                OpenContainingFolder(dlg.FileName);
+            }
         }
+
+        private void OpenContainingFolder(string fileName)
+        {
+            // var folder = Path.GetDirectoryName(fileName);
+            var psi = new ProcessStartInfo("Explorer.exe", "/select," + fileName);
+            Process.Start(psi);
+        }
+
 
         private void SaveReport_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var dlg = new SaveFileDialog();
+            dlg.Filter = ".html files|*.html";
+            dlg.DefaultExt = ".html";
+            if (dlg.ShowDialog(this).Value)
+            {
+                vm.SaveReport(dlg.FileName);
+                OpenContainingFolder(dlg.FileName);
+            }
+        }
+
+        private void CopySvg_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(vm.Model.ToSvg(plot1.ActualWidth, plot1.ActualHeight, true));
+        }
+
+        private void CopyBitmap_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetImage(plot1.ToBitmap());
+        }
+
+        private void CopyXaml_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(plot1.ToXaml());
         }
 
         private void HelpAbout_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MessageBox.Show("by OxyPlot");
         }
 
         private void HelpWeb_Click(object sender, RoutedEventArgs e)
         {
             Process.Start("http://oxyplot.codeplex.com");
         }
+
     }
 }
