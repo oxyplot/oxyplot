18a24a33ea7f4bfd24b744772b57b68b6eb2c124
objo <objo@localhost>
2011-08-14 21:09:37 +0000

    OxyPlot: Added StairStep, Stem, CandleStick and HighLowSeries. Examples added to the Example browser (both WPF and Silverlight).

---
+++ Source/Examples/ExampleLibrary/Examples/BarSeriesExamples.cs
@@ -0,0 +1,67 @@
+﻿using OxyPlot;
+
+namespace ExampleLibrary
+{
+    [Examples("BarSeries")]
+    public static class BarSeriesExamples
+    {
+        [Example("Simple BarSeries")]
+        public static PlotModel SimpleBarSeries()
+        {
+            var model = CreateBarSeriesModel(false, false);
+            return model;
+        }
+
+        [Example("Stacked BarSeries")]
+        public static PlotModel StakcedBarSeries()
+        {
+            var model = CreateBarSeriesModel(true, false);
+            return model;
+        }
+
+        [Example("Simple Horizontal BarSeries")]
+        public static PlotModel SimpleHorizontalBarSeries()
+        {
+            var model = CreateBarSeriesModel(false, true);
+            return model;
+        }
+
+        [Example("Stacked Horizontal BarSeries")]
+        public static PlotModel StakcedHorizontalBarSeries()
+        {
+            var model = CreateBarSeriesModel(true, true);
+            return model;
+        }
+
+        private static PlotModel CreateBarSeriesModel(bool stacked, bool horizontal)
+        {
+            var model = new PlotModel("BarSeries")
+                          {
+                              LegendPlacement = LegendPlacement.Outside,
+                              LegendPosition = LegendPosition.BottomCenter,
+                              LegendOrientation = LegendOrientation.Horizontal,
+                              LegendBorderThickness = 0
+                          };
+            var s1 = new BarSeries { Title = "BarSeries 1", IsStacked = stacked, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s1.Values.Add(25);
+            s1.Values.Add(37);
+            s1.Values.Add(18);
+            s1.Values.Add(40);
+            var s2 = new BarSeries { Title = "BarSeries 2", IsStacked = stacked, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s2.Values.Add(12);
+            s2.Values.Add(14);
+            s2.Values.Add(20);
+            s2.Values.Add(26);
+            var categoryAxis = new CategoryAxis { Position = horizontal ? AxisPosition.Left : AxisPosition.Bottom };
+            categoryAxis.Labels.Add("Category A");
+            categoryAxis.Labels.Add("Category B");
+            categoryAxis.Labels.Add("Category C");
+            categoryAxis.Labels.Add("Category D");
+            model.Series.Add(s1);
+            model.Series.Add(s2);
+            model.Axes.Add(categoryAxis);
+            model.Axes.Add(new LinearAxis(horizontal ? AxisPosition.Bottom : AxisPosition.Left) { MinimumPadding = 0, AbsoluteMinimum = 0 });
+            return model;
+        }
+    }
+}
---
+++ Source/OxyPlot/Series/StemSeries.cs
@@ -0,0 +1,211 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StemSeries.cs" company="OxyPlot">
+//   http://oxyplot.codeplex.com
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OxyPlot
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// StemSeries is used to plot discrete data in a stemplot.
+    /// http://en.wikipedia.org/wiki/Stemplot
+    /// http://www.mathworks.com/help/techdoc/ref/stem.html
+    /// </summary>
+    public class StemSeries : LineSeries
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StemSeries"/> class.
+        /// </summary>
+        public StemSeries()
+        {
+            this.Base = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StemSeries"/> class. 
+        /// </summary>
+        /// <param name="title">
+        /// The title.
+        /// </param>
+        public StemSeries(string title)
+            : base(title)
+        {
+            this.Title = title;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StemSeries"/> class. 
+        /// </summary>
+        /// <param name="color">
+        /// The color of the line stroke.
+        /// </param>
+        /// <param name="strokeThickness">
+        /// The stroke thickness (optional).
+        /// </param>
+        /// <param name="title">
+        /// The title (optional).
+        /// </param>
+        public StemSeries(OxyColor color, double strokeThickness = 1, string title = null)
+            : base(color, strokeThickness, title)
+        {
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets Base.
+        /// </summary>
+        public double Base { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the point on the curve that is nearest the specified point.
+        /// </summary>
+        /// <param name="point">
+        /// The point.
+        /// </param>
+        /// <param name="interpolate">
+        /// The interpolate.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public override TrackerHitResult GetNearestPoint(ScreenPoint point, bool interpolate)
+        {
+            if (interpolate)
+            {
+                return null;
+            }
+
+            TrackerHitResult result = null;
+
+            // http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/
+            double minimumDistance = double.MaxValue;
+
+            for (int i = 0; i < this.points.Count; i++)
+            {
+                DataPoint p1 = this.points[i];
+                var basePoint = new DataPoint(p1.X, this.Base);
+                ScreenPoint sp1 = AxisBase.Transform(p1, this.XAxis, this.YAxis);
+                ScreenPoint sp2 = AxisBase.Transform(basePoint, this.XAxis, this.YAxis);
+
+                double sp21X = sp2.x - sp1.x;
+                double sp21Y = sp2.y - sp1.y;
+                double u1 = (point.x - sp1.x) * sp21X + (point.y - sp1.y) * sp21Y;
+                double u2 = sp21X * sp21X + sp21Y * sp21Y;
+                double ds = sp21X * sp21X + sp21Y * sp21Y;
+
+                if (ds < 4)
+                {
+                    // if the points are very close, we can get numerical problems, just use the first point...
+                    u1 = 0;
+                    u2 = 1;
+                }
+
+                if (u2 == 0)
+                {
+                    continue; // P1 && P2 coincident
+                }
+
+                double u = u1 / u2;
+                if (u < 0 || u > 1)
+                {
+                    continue; // outside line
+                }
+
+                double sx = sp1.x + u * sp21X;
+                double sy = sp1.y + u * sp21Y;
+
+                double dx = point.x - sx;
+                double dy = point.y - sy;
+                double distance = dx * dx + dy * dy;
+
+                if (distance < minimumDistance)
+                {
+                    result = new TrackerHitResult(
+                        this, new DataPoint(p1.x, p1.y), new ScreenPoint(sp1.x, sp1.y), this.GetItem(i), null);
+                    minimumDistance = distance;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Renders the LineSeries on the specified rendering context.
+        /// </summary>
+        /// <param name="rc">
+        /// The rendering context.
+        /// </param>
+        /// <param name="model">
+        /// The owner plot model.
+        /// </param>
+        public override void Render(IRenderContext rc, PlotModel model)
+        {
+            if (this.points.Count == 0)
+            {
+                return;
+            }
+
+            Debug.Assert(this.XAxis != null && this.YAxis != null, "Axis has not been defined.");
+
+            double minDistSquared = this.MinimumSegmentLength * this.MinimumSegmentLength;
+
+            OxyRect clippingRect = this.GetClippingRect();
+
+            // Transform all points to screen coordinates
+            // Render the line when invalid points occur
+            var markerPoints = new List<ScreenPoint>();
+            foreach (DataPoint point in this.points)
+            {
+                if (!this.IsValidPoint(point, this.XAxis, this.YAxis))
+                {
+                    continue;
+                }
+
+                var basePoint = new DataPoint(point.X, this.Base);
+                ScreenPoint p0 = this.XAxis.Transform(basePoint, this.YAxis);
+                ScreenPoint p1 = this.XAxis.Transform(point, this.YAxis);
+
+                if (this.StrokeThickness > 0 && this.LineStyle != LineStyle.None)
+                {
+                    rc.DrawClippedLine(
+                        new[] { p0, p1 }, 
+                        clippingRect, 
+                        minDistSquared, 
+                        this.Color, 
+                        this.StrokeThickness, 
+                        this.LineStyle, 
+                        this.LineJoin, 
+                        false);
+                }
+
+                markerPoints.Add(p1);
+            }
+
+            if (this.MarkerType != MarkerType.None)
+            {
+                rc.DrawMarkers(
+                    markerPoints.ToArray(), 
+                    clippingRect, 
+                    this.MarkerType, 
+                    this.MarkerOutline, 
+                    new[] { this.MarkerSize }, 
+                    this.MarkerFill, 
+                    this.MarkerStroke, 
+                    this.MarkerStrokeThickness);
+            }
+        }
+
+        #endregion
+    }
+}
