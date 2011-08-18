b4407d81cb9a3304bc24eafe79b5d85dd7af1ef0
objo <objo@localhost>
2011-08-18 06:11:03 +0000

    Added OxyPlot.Wpf.Tests - unit tests that check that default values in the OxyPlot.WPF and OxyPlot classes are equal

---
+++ Source/OxyPlot.Wpf.Tests/AxesTests.cs
@@ -0,0 +1,46 @@
+﻿namespace OxyPlot.Wpf.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AxesTests
+    {
+        [Test]
+        public void LinearAxis()
+        {
+            var s1 = new OxyPlot.LinearAxis();
+            var s2 = new OxyPlot.Wpf.LinearAxis();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+
+        [Test]
+        public void LogarithmicAxis()
+        {
+            var s1 = new OxyPlot.LogarithmicAxis();
+            var s2 = new OxyPlot.Wpf.LogarithmicAxis();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void DateTimeAxis()
+        {
+            var s1 = new OxyPlot.DateTimeAxis();
+            var s2 = new OxyPlot.Wpf.DateTimeAxis();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void TimeSpanAxis()
+        {
+            var s1 = new OxyPlot.TimeSpanAxis();
+            var s2 = new OxyPlot.Wpf.TimeSpanAxis();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+
+        [Test]
+        public void CategoryAxis()
+        {
+            var s1 = new OxyPlot.CategoryAxis();
+            var s2 = new OxyPlot.Wpf.CategoryAxis();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+    }
+}
---
+++ Source/OxyPlot.Wpf.Tests/SeriesTests.cs
@@ -0,0 +1,83 @@
+﻿namespace OxyPlot.Wpf.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SeriesTests
+    {
+        [Test]
+        public void LineSeries()
+        {
+            var s1 = new OxyPlot.LineSeries();
+            var s2 = new OxyPlot.Wpf.LineSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+
+        [Test]
+        public void AreaSeries()
+        {
+            var s1 = new OxyPlot.AreaSeries();
+            var s2 = new OxyPlot.Wpf.AreaSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void BarSeries()
+        {
+            var s1 = new OxyPlot.BarSeries();
+            var s2 = new OxyPlot.Wpf.BarSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void ScatterSeries()
+        {
+            var s1 = new OxyPlot.ScatterSeries();
+            var s2 = new OxyPlot.Wpf.ScatterSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        /*
+        [Test]
+        public void CandleStickSeries()
+        {
+            var s1 = new OxyPlot.CandleStickSeries();
+            var s2 = new OxyPlot.Wpf.CandleStickSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void FunctionSeries()
+        {
+            var s1 = new OxyPlot.FunctionSeries();
+            var s2 = new OxyPlot.Wpf.FunctionSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void HighLowSeries()
+        {
+            var s1 = new OxyPlot.HighLowSeries();
+            var s2 = new OxyPlot.Wpf.HighLowSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void PieSeries()
+        {
+            var s1 = new OxyPlot.PieSeries();
+            var s2 = new OxyPlot.Wpf.PieSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void StairStepSeries()
+        {
+            var s1 = new OxyPlot.StairStepSeries();
+            var s2 = new OxyPlot.Wpf.StairStepSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }
+        [Test]
+        public void StemSeries()
+        {
+            var s1 = new OxyPlot.StemSeries();
+            var s2 = new OxyPlot.Wpf.StemSeries();
+            Assert.PropertiesAreEqual(s1, s2);
+        }*/
+
+       
+    }
+}
