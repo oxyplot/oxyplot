f563e80b9f3fa918862d82150bf2096d1ac89618
Oystein Bjorke <oystein.bjorke@gmail.com>
2011-08-18 06:00:38 +0000

    LinearAxis: support for units/fractions on axes (#8845) e.g. instead of numbers you can show 0, ?/2, ?, 3?/2, 2?

--- Source/OxyPlot/Axes/LinearAxis.cs
+++ Source/OxyPlot/Axes/LinearAxis.cs
@@ -6,10 +6,33 @@
     public class LinearAxis : AxisBase
     {
         /// <summary>
+        /// Gets or sets a value indicating whether to format numbers as fractions.
+        /// </summary>
+        public bool FormatAsFractions { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fraction unit.
+        /// </summary>
+        /// <value>The fraction unit.</value>
+        public double FractionUnit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fraction unit symbol.
+        /// Use FractionUnit = Math.PI and FractionUnitSymbol = "π" if you want the axis to show "π/2,π,3π/2,2π" etc.
+        /// Use FractionUnit = 1 and FractionUnitSymbol = "L" if you want the axis to show "0,L/2,L" etc.
+        /// </summary>
+        /// <value>The fraction unit symbol.</value>
+        public string FractionUnitSymbol { get; set; }
+
+
+        /// <summary>
         ///   Initializes a new instance of the <see cref = "LinearAxis" /> class.
         /// </summary>
         public LinearAxis()
         {
+            FractionUnit = 1.0;
+            FractionUnitSymbol = null;
+            FormatAsFractions = false;
         }
 
         /// <summary>
@@ -38,6 +61,12 @@
             Maximum = maximum;
             MajorStep = majorStep;
             MinorStep = minorStep;
+        }
+
+        public override string FormatValue(double x)
+        {
+            if (FormatAsFractions) return FractionHelper.ConvertToFractionString(x, FractionUnit, FractionUnitSymbol, 1e-6);
+            return base.FormatValue(x);
         }
     }
 }
