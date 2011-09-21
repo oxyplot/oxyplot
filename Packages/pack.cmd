096f1bfdeb819efb0a982e26b1d8357946467956
objo <objo@localhost>
2011-09-21 19:16:36 +0000

    Removed a file from the nuget package that shouldn't be there

--- Packages/pack.cmd
+++
@@ -1,7 +0,0 @@
-cd OxyPlot.Wpf
-copy ..\..\source\OxyPlot.Wpf\bin\Release\*.dll lib
-..\..\tools\nuget.exe pack OxyPlot.Wpf.nuspec
-cd ..\OxyPlot.Silverlight
-copy ..\..\source\OxyPlot.Silverlight\bin\Release\*.dll lib
-..\..\tools\nuget.exe pack OxyPlot.Silverlight.nuspec
-cd ..
