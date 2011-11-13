1dbfec7d289dcbb55592c82319c83762d3ae9744
Oystein Bjorke <oystein.bjorke@gmail.com>
2011-11-13 18:38:14 +0000

    Corrected error in nuget pack script

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -2,10 +2,10 @@ copy ..\Output\OxyPlot.dll ..\Packages\OxyPlot.Wpf\lib
 copy ..\Output\OxyPlot.xml ..\Packages\OxyPlot.Wpf\lib
 copy ..\Output\OxyPlot.Wpf.dll ..\Packages\OxyPlot.Wpf\lib
 copy ..\Output\OxyPlot.Wpf.xml ..\Packages\OxyPlot.Wpf\lib
-nuget.exe pack ..\Packages\OxyPlot.Wpf\OxyPlot.Wpf.nuspec
+nuget.exe pack ..\Packages\OxyPlot.Wpf\OxyPlot.Wpf.nuspec -OutputDirectory ..\Packages > pack.log
 
 copy ..\Output\OxyPlotSL.dll ..\Packages\OxyPlot.Silverlight\lib
 copy ..\Output\OxyPlotSL.xml ..\Packages\OxyPlot.Silverlight\lib
 copy ..\Output\OxyPlot.Silverlight.dll ..\Packages\OxyPlot.Silverlight\lib
 copy ..\Output\OxyPlot.Silverlight.xml ..\Packages\OxyPlot.Silverlight\lib
-nuget.exe pack ..\Packages\OxyPlot.Silverlight\OxyPlot.Silverlight.nuspec
+nuget.exe pack ..\Packages\OxyPlot.Silverlight\OxyPlot.Silverlight.nuspec -OutputDirectory ..\Packages > pack.log
