ab5545b5fad7d8e88e3120da06f032825c0e5275
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-01-26 06:06:50 +0000

    LineAnnotation: Added ClipByXAxis and ClipByYAxis properties. (Issue #9815)

--- Source/Examples/ExampleLibrary/Examples/AnnotationExamples.cs
+++ Source/Examples/ExampleLibrary/Examples/AnnotationExamples.cs
@@ -14,7 +14,7 @@ namespace ExampleLibrary
         [Example("Linear axes")]
         public static PlotModel LinearAxes()
         {
-            var model = new PlotModel("Annotations");
+            var model = new PlotModel("Annotations on linear axes");
             model.Axes.Add(new LinearAxis(AxisPosition.Bottom, -20, 80));
             model.Axes.Add(new LinearAxis(AxisPosition.Left, -10, 10));
             model.Annotations.Add(new LineAnnotation() { Slope = 0.1, Intercept = 1, Text = "First" });
@@ -23,6 +23,7 @@ namespace ExampleLibrary
             model.Annotations.Add(new LineAnnotation() { Type = LineAnnotationType.Horizontal, Y = 2, MaximumX = 4, Color = OxyColors.Gold, Text = "Horizontal" });
             return model;
         }
+
 
         [Example("Logarithmic axes")]
         public static PlotModel LogarithmicAxes()
@@ -34,6 +35,22 @@ namespace ExampleLibrary
             model.Annotations.Add(new LineAnnotation() { Slope = 0.3, Intercept = 2, MaximumX = 40, Color = OxyColors.Red, Text = "Second" });
             model.Annotations.Add(new LineAnnotation() { Type = LineAnnotationType.Vertical, X = 4, MaximumY = 10, Color = OxyColors.Green, Text = "Vertical" });
             model.Annotations.Add(new LineAnnotation() { Type = LineAnnotationType.Horizontal, Y = 2, MaximumX = 4, Color = OxyColors.Gold, Text = "Horizontal" });
+            return model;
+        }
+
+        [Example("No clipping")]
+        public static PlotModel LinearAxesMultipleAxes()
+        {
+            var model = new PlotModel("ClipByXAxis = false, ClipByYAxis = false");
+            model.Axes.Add(new LinearAxis(AxisPosition.Bottom, -20, 80) { StartPosition = 0, EndPosition = 0.5 });
+            model.Axes.Add(new LinearAxis(AxisPosition.Left, -10, 10) { StartPosition = 0, EndPosition = 0.5 });
+            model.Axes.Add(new LinearAxis(AxisPosition.Bottom, -20, 80) { StartPosition = 0.5, EndPosition = 1 });
+            model.Axes.Add(new LinearAxis(AxisPosition.Left, -10, 10) { StartPosition = 0.5, EndPosition = 1 });
+
+            //  model.Annotations.Add(new LineAnnotation() { ClipByXAxis = false, ClipByYAxis = false, Slope = 0.1, Intercept = 1, Text = "First" });
+            //  model.Annotations.Add(new LineAnnotation() { ClipByXAxis = false, ClipByYAxis = false, Slope = 0.3, Intercept = 2, MaximumX = 40, Color = OxyColors.Red, Text = "Second" });
+            model.Annotations.Add(new LineAnnotation { ClipByYAxis = false, Type = LineAnnotationType.Vertical, X = 0, Color = OxyColors.Green, Text = "Vertical" });
+            model.Annotations.Add(new LineAnnotation { ClipByXAxis = false, Type = LineAnnotationType.Horizontal, Y = 2, Color = OxyColors.Gold, Text = "Horizontal" });
             return model;
         }
     }
