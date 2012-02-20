b6302e71f37571aa145008fbe41e221af11845df
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-02-20 22:33:37 +0000

    Moved HistogramTest from branches to trunk/Source/Examples/Silverlight

---
+++ Source/Examples/Silverlight/HistogramTest/OxyPlotChart.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using OxyPlot.Silverlight;
+
+namespace Visiblox.Charts.Examples
+{
+    public partial class OxyPlotChart : UserControl
+    {
+        public OxyPlotChart()
+        {
+            InitializeComponent();
+        }
+
+        public Plot Chart
+        {
+            get { return chart; }
+        }
+    }
+}
