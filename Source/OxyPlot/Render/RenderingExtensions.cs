b65482a054cc29efaf9bde2871cc1f57a3e2687a
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-03-12 01:28:02 +0100

    RenderingExtension: added DrawClippedText extension method

--- Source/OxyPlot/Render/RenderingExtensions.cs
+++ Source/OxyPlot/Render/RenderingExtensions.cs
@@ -309,6 +309,41 @@ namespace OxyPlot
         }
 
         /// <summary>
+        /// Draws the clipped text.
+        /// </summary>
+        /// <param name="rc">The rendering context.</param>
+        /// <param name="clippingRectangle">The clipping rectangle.</param>
+        /// <param name="p">The position.</param>
+        /// <param name="text">The text.</param>
+        /// <param name="fill">The fill color.</param>
+        /// <param name="fontFamily">The font family.</param>
+        /// <param name="fontSize">Size of the font.</param>
+        /// <param name="fontWeight">The font weight.</param>
+        /// <param name="rotate">The rotation angle.</param>
+        /// <param name="horizontalAlignment">The horizontal align.</param>
+        /// <param name="verticalAlignment">The vertical align.</param>
+        /// <param name="maxSize">Size of the max.</param>
+        public static void DrawClippedText(
+            this IRenderContext rc,
+            OxyRect clippingRectangle,
+            ScreenPoint p,
+            string text, 
+            OxyColor fill,
+            string fontFamily = null,
+            double fontSize = 10,
+            double fontWeight = 500,
+            double rotate = 0,
+            HorizontalTextAlign horizontalAlignment = HorizontalTextAlign.Left,
+            VerticalTextAlign verticalAlignment = VerticalTextAlign.Top,
+            OxySize? maxSize = null)
+        {
+            if (clippingRectangle.Contains(p.X, p.Y))
+            {
+                rc.DrawText(p, text, fill, fontFamily, fontSize, fontWeight, rotate, horizontalAlignment, verticalAlignment, maxSize);
+            }
+        }
+
+        /// <summary>
         /// Draws a line specified by coordinates.
         /// </summary>
         /// <param name="rc">
