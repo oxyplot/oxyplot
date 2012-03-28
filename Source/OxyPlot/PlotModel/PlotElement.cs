56c8ccdaa064d0610249f20d64ac8df7c47f2a22
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-03-28 07:35:46 +0200

    PlotElement: added Tag property

--- Source/OxyPlot/PlotModel/PlotElement.cs
+++ Source/OxyPlot/PlotModel/PlotElement.cs
@@ -14,5 +14,17 @@ namespace OxyPlot
     [Serializable]
     public abstract class PlotElement
     {
+        /// <summary>
+        /// Gets or sets an arbitrary object value that can be used to store custom information about this plot element.
+        /// </summary>
+        /// <value>
+        /// The intended value. This property has no default value.
+        /// </value>
+        /// <remarks>
+        /// This property is analogous to Tag properties in other Microsoft programming models. 
+        /// Tag is intended to provide a pre-existing property location where you can store some basic custom information 
+        /// about any PlotElement without requiring you to subclass an element.
+        /// </remarks>
+        public object Tag { get; set; }
     }
 }
