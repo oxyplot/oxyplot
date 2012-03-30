f80f5b32cd2e763c68979afa602ed7d80e57e97f
OBJO <OBJO@OSLLP9329.verit.dnv.com>
2012-03-30 20:48:21 +0200

    Added WindowsForms nuget package

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -11,3 +11,10 @@ copy ..\Output\OxyPlotSL.xml ..\Packages\OxyPlot.Silverlight\lib
 copy ..\Output\OxyPlot.Silverlight.dll ..\Packages\OxyPlot.Silverlight\lib
 copy ..\Output\OxyPlot.Silverlight.xml ..\Packages\OxyPlot.Silverlight\lib
 nuget.exe pack ..\Packages\OxyPlot.Silverlight\OxyPlot.Silverlight.nuspec -OutputDirectory ..\Packages >> pack.log
+
+mkdir ..\Packages\OxyPlot.WindowsForms\lib
+copy ..\Output\OxyPlot.dll ..\Packages\OxyPlot.WindowsForms\lib
+copy ..\Output\OxyPlot.xml ..\Packages\OxyPlot.WindowsForms\lib
+copy ..\Output\OxyPlot.WindowsForms.dll ..\Packages\OxyPlot.WindowsForms\lib
+copy ..\Output\OxyPlot.WindowsForms.xml ..\Packages\OxyPlot.WindowsForms\lib
+nuget.exe pack ..\Packages\OxyPlot.WindowsForms\OxyPlot.WindowsForms.nuspec -OutputDirectory ..\Packages >> pack.log
