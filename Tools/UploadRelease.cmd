686190f62894917f1e1ec57e972e18c56ad48118
objo <objo@localhost>
2012-06-02 17:14:28 +0200

    Tools: added UploadRelease script

---
+++ Tools/UploadRelease.cmd
@@ -0,0 +1,1 @@
+CodeplexReleaseUploader.exe /Project=oxyplot "/Release=OxyPlot %1" "/Description=Release assemblies and examples." "/Upload=..\Output\OxyPlot-Release-%1.zip,OxyPlot-Release-%1.zip" "/Upload=..\Output\OxyPlot-Examples-%1.zip,OxyPlot-Examples-%1.zip" > UploadRelease.log
