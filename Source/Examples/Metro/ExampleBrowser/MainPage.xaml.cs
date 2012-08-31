7fee705e90e912f07436edb74f0ea6527c76ff0b
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-08-31 06:39:33 +0200

    Metro project refactoring

--- Source/Examples/Metro/ExampleBrowser/MainPage.xaml.cs
+++ Source/Examples/Metro/ExampleBrowser/MainPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace ExampleBrowserBasic
+namespace ExampleBrowser
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
