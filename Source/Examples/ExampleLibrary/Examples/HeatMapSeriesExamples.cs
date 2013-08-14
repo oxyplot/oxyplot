c7b1f45b32975c755fed8f7972edae93e4531003
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-08-14 08:25:19 +0200

    HeatMapSeries: corrected min/max calculation, added Interpolated property Added examples [discussion:453174]

--- Source/Examples/ExampleLibrary/Examples/HeatMapSeriesExamples.cs
+++ Source/Examples/ExampleLibrary/Examples/HeatMapSeriesExamples.cs
@@ -46,8 +46,8 @@ namespace ExampleLibrary
             Func<double, double, double> peaks = (x, y) => 3 * (1 - x) * (1 - x) * Math.Exp(-(x * x) - (y + 1) * (y + 1))
                - 10 * (x / 5 - x * x * x - y * y * y * y * y) * Math.Exp(-x * x - y * y)
                - 1.0 / 3 * Math.Exp(-(x + 1) * (x + 1) - y * y);
-            var xvalues = ArrayHelper.CreateVector(x0, x1, 0.03);
-            var yvalues = ArrayHelper.CreateVector(y0, y1, 0.03);
+            var xvalues = ArrayHelper.CreateVector(x0, x1, 100);
+            var yvalues = ArrayHelper.CreateVector(y0, y1, 100);
             var peaksData = ArrayHelper.Evaluate(peaks, xvalues, yvalues);
 
             var model = new PlotModel("Peaks");
@@ -66,6 +66,36 @@ namespace ExampleLibrary
                     Data = peaksData
                 };
             model.Series.Add(cs);
+            return model;
+        }
+
+        [Example("Coordinates")]
+        public static PlotModel Coordinates()
+        {
+            var data = new double[2, 3];
+            data[0, 0] = 0;
+            data[0, 1] = 0.2;
+            data[0, 2] = 0.4;
+            data[1, 0] = 0.1;
+            data[1, 1] = 0.3;
+            data[1, 2] = 0.2;
+
+            var model = new PlotModel("Coordinates example", "Bounding box should be [0,2] and [0,3]");
+            model.Axes.Add(new ColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Jet(500), HighColor = OxyColors.Gray, LowColor = OxyColors.Black });
+
+            // adding half cellwidth/cellheight to bounding box coordinates
+            var hms = new HeatMapSeries { X0 = 0.5, X1 = 1.5, Y0 = 0.5, Y1 = 2.5, Data = data };
+            model.Series.Add(hms);
+            return model;
+        }
+
+        [Example("Not interpolated")]
+        public static PlotModel NotInterpolated()
+        {
+            var model = Coordinates();
+            model.Title = "Not interpolated values";
+            var hms = (HeatMapSeries)model.Series[0];
+            hms.Interpolate = false;
             return model;
         }
     }
