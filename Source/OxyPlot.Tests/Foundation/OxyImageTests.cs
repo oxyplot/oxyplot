93f498df68e93f7f9ad6d8442de45a1c3cbf4831
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-08-22 21:52:04 +0200

    Modified OxyImage/Palette unit test [discussion:453825]

--- Source/OxyPlot.Tests/Foundation/OxyImageTests.cs
+++ Source/OxyPlot.Tests/Foundation/OxyImageTests.cs
@@ -106,15 +106,16 @@
         public void Discussion453825()
         {
             var data = new byte[100 * 100];
+            int k = 0;
             for (int i = 0; i < 100; i++)
             {
                 for (int j = 0; j < 100; j++)
                 {
-                    data[i + (j * 100)] = (byte)(i + j);
+                    data[i + (j * 100)] = (byte)(k++ % 256);
                 }
             }
 
-            var palette = OxyPalettes.Gray(255).Colors.ToArray();
+            var palette = OxyPalettes.Gray(256).Colors.ToArray();
             var im = OxyImage.FromIndexed8(100, 100, data, palette);
             File.WriteAllBytes("Discussion453825.bmp", im.GetData());
         }
