e1f867f2af11c4069373be82da394ffbbdb97d0b
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-09-17 21:13:27 +0200

    Wpf: Changed Culture property to type CultureInfo

--- Source/OxyPlot.Wpf/Plot.Properties.cs
+++ Source/OxyPlot.Wpf/Plot.Properties.cs
@@ -1,9 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Plot.Properties.cs" company="OxyPlot">
 //   The MIT License (MIT)
-//
+//   
 //   Copyright (c) 2012 Oystein Bjorke
-//
+//   
 //   Permission is hereby granted, free of charge, to any person obtaining a
 //   copy of this software and associated documentation files (the
 //   "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 //   distribute, sublicense, and/or sell copies of the Software, and to
 //   permit persons to whom the Software is furnished to do so, subject to
 //   the following conditions:
-//
+//   
 //   The above copyright notice and this permission notice shall be included
 //   in all copies or substantial portions of the Software.
-//
+//   
 //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -27,10 +27,9 @@
 //   Represents the WPF Plot control.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace OxyPlot.Wpf
 {
-    using System;
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Globalization;
     using System.Windows;
@@ -56,7 +55,7 @@ namespace OxyPlot.Wpf
         /// The culture property.
         /// </summary>
         public static readonly DependencyProperty CultureProperty = DependencyProperty.Register(
-            "Culture", typeof(IFormatProvider), typeof(Plot), new UIPropertyMetadata(null, AppearanceChanged));
+            "Culture", typeof(CultureInfo), typeof(Plot), new UIPropertyMetadata(null, AppearanceChanged));
 
         /// <summary>
         /// The default tracker property.
@@ -454,11 +453,11 @@ namespace OxyPlot.Wpf
         /// <summary>
         /// Gets or sets Culture.
         /// </summary>
-        public IFormatProvider Culture
+        public CultureInfo Culture
         {
             get
             {
-                return (IFormatProvider)this.GetValue(CultureProperty);
+                return (CultureInfo)this.GetValue(CultureProperty);
             }
 
             set
@@ -1256,7 +1255,7 @@ namespace OxyPlot.Wpf
                 m.PlotMargins = this.PlotMargins.ToOxyThickness();
                 m.AutoAdjustPlotMargins = this.AutoAdjustPlotMargins;
 
-                // m.Culture = this.Culture;
+                m.Culture = this.Culture;
 
                 m.Padding = this.Padding.ToOxyThickness();
                 m.TitleFont = this.TitleFont;
