581fe9d5ca05cdff198c64d67c598950e010a1ff
Xavier <Xavier@xavier-PC>
2013-09-23 15:04:10 +0200

    - Removed a bunch of StyleCop errors - Implemented the FunctionAnnotation, finished separating FunctionAnnotation and LineAnnotation.

--- Source/OxyPlot/Annotations/FunctionAnnotationType.cs
+++ Source/OxyPlot/Annotations/FunctionAnnotationType.cs
@@ -1,9 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="FunctionLineAnnotationType.cs" company="OxyPlot">
+// <copyright file="FunctionAnnotationType.cs" company="OxyPlot">
 //   The MIT License (MIT)
-//
+//   
 //   Copyright (c) 2012 Oystein Bjorke
-//
+//   
 //   Permission is hereby granted, free of charge, to any person obtaining a
 //   copy of this software and associated documentation files (the
 //   "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 //   distribute, sublicense, and/or sell copies of the Software, and to
 //   permit persons to whom the Software is furnished to do so, subject to
 //   the following conditions:
-//
+//   
 //   The above copyright notice and this permission notice shall be included
 //   in all copies or substantial portions of the Software.
-//
+//   
 //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -24,7 +24,7 @@
 //   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 // <summary>
-//   The line annotation type.
+//   The function annotation type.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace OxyPlot.Annotations
@@ -34,11 +34,6 @@ namespace OxyPlot.Annotations
     /// </summary>
     public enum FunctionAnnotationType
     {
-        /// <summary>
-        /// Linear equation y=mx+b given by the Slope and Intercept properties
-        /// </summary>
-        LinearEquation,
-
         /// <summary>
         /// Curve equation x=f(y) given by the Equation property
         /// </summary>
