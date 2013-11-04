6ac1ba8e91b7404110c35bfc485e9ed7a64838e7
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-11-05 00:43:57 +0100

    OxyPlot.WindowsForms: added CLSCompliant attribute

--- Source/OxyPlot.WindowsForms/Properties/AssemblyInfo.cs
+++ Source/OxyPlot.WindowsForms/Properties/AssemblyInfo.cs
@@ -4,7 +4,10 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
 using System.Reflection;
 
 [assembly: AssemblyTitle("OxyPlot for Windows Forms")]
 [assembly: AssemblyDescription("OxyPlot controls for Windows Forms.")]
+
+[assembly: CLSCompliant(true)]
