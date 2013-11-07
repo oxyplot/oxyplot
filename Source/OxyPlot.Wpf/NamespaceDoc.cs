36cabb4d617ec20da0f59b90b338ae9a693fc1e7
objo <oystein.bjorke@gmail.com>
2013-11-07 23:07:32 +0100

    Added missing NamespaceDocs

--- Source/OxyPlot.Wpf/NamespaceDoc.cs
+++ Source/OxyPlot.Wpf/NamespaceDoc.cs
@@ -28,7 +28,7 @@
 namespace OxyPlot.Wpf
 {
     /// <summary>
-    /// The OxyPlot.Wpf namespace contains WPF controls and classes providing output to bitmap and flow documents.
+    /// The OxyPlot.Wpf namespace contains WPF controls and PNG export functionality based on WPF.
     /// </summary>
     [System.Runtime.CompilerServices.CompilerGenerated]
     internal class NamespaceDoc
