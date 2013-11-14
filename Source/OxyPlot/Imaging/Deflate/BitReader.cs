55119bc15d4ba58e18c48498f5378ef729f183b8
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-11-14 21:52:48 +0100

    OxyImage: change definition of pixels. Indexing is now [x,y] where [0,0] is upper-left.
    PngDecoder: Add support for DEFLATE decompression (not working properly yet)

---
+++ Source/OxyPlot/Imaging/Deflate/BitReader.cs
@@ -0,0 +1,62 @@
+﻿namespace OxyPlot
+{
+    using System.IO;
+
+    /// <summary>
+    /// Implements a binary reader that can read bits.
+    /// </summary>
+    public abstract class BitReader
+    {
+        /// <summary>
+        /// Reads a byte from the stream.
+        /// </summary>
+        /// <returns>The byte.</returns>
+        public abstract int Read();
+
+        /// <summary>
+        /// Reads a bit from the stream. 
+        /// </summary>
+        /// <returns>Returns 0 or 1 if a bit is available, or throws an EOFException if the end of stream is reached.</returns>
+        public abstract int ReadNoEof();
+
+        /// <summary>
+        /// Closes this stream and the underlying InputStream.
+        /// </summary>
+        public abstract void Close();
+
+        /// <summary>
+        /// Returns the current bit position, which is between 0 and 7 inclusive. The number of bits remaining in the current byte is 8 minus this number.
+        /// </summary>
+        /// <returns>The bit position.</returns>
+        public abstract int GetBitPosition();
+
+        /// <summary>
+        /// Discards the remainder of the current byte and reads the next byte from the stream.
+        /// </summary>
+        /// <returns>The byte.</returns>
+        public abstract int ReadByte();
+
+        /// <summary>
+        /// Reads the specified number of bits.
+        /// </summary>
+        /// <param name="bits">The number of bits.</param>
+        /// <returns>The bits.</returns>
+        /// <exception cref="System.IO.IOException">Reading past EOF.</exception>
+        public int ReadBits(int bits)
+        {
+            int r = 0;
+            for (int i = 0; i < bits; i++)
+            {
+                var bit = this.Read();
+                if (bit == -1)
+                {
+                    throw new IOException();
+                }
+
+                r += bit << i;
+            }
+
+            return r;
+        }
+    }
+}
---
+++ Source/OxyPlot/Imaging/Deflate/CircularDictionary.cs
@@ -0,0 +1,94 @@
+namespace OxyPlot
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Provides a circular dictionary.
+    /// </summary>
+    /// <remarks>
+    /// The code is a c# port of Nayuki Minase's DEFLATE project at <a href="https://github.com/nayuki/DEFLATE">github</a>.
+    /// Original source code: <a href="https://github.com/nayuki/DEFLATE/blob/master/src/nayuki/deflate/CircularDictionary.java">CircularDictionary.java</a>.
+    /// </remarks>
+    internal class CircularDictionary
+    {
+        /// <summary>
+        /// The data
+        /// </summary>
+        private readonly byte[] data;
+
+        /// <summary>
+        /// The mask
+        /// </summary>
+        private readonly int mask;
+
+        /// <summary>
+        /// The index
+        /// </summary>
+        private int index;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularDictionary"/> class.
+        /// </summary>
+        /// <param name="size">The size of the dictionary.</param>
+        public CircularDictionary(int size)
+        {
+            this.data = new byte[size];
+            this.index = 0;
+
+            // Test if size is a power of 2
+            if (size > 0 && (size & (size - 1)) == 0)
+            {
+                this.mask = size - 1;
+            }
+            else
+            {
+                this.mask = 0;
+            }
+        }
+
+        public void Append(int b)
+        {
+            this.data[this.index] = (byte)b;
+            if (this.mask != 0)
+            {
+                this.index = (this.index + 1) & this.mask;
+            }
+            else
+            {
+                this.index = (this.index + 1) % this.data.Length;
+            }
+        }
+
+        public void Copy(int dist, int len, BinaryWriter w)
+        {
+            if (len < 0 || dist < 1 || dist > this.data.Length)
+            {
+                throw new Exception();
+            }
+
+            if (this.mask != 0)
+            {
+                int readIndex = (this.index - dist + this.data.Length) & this.mask;
+                for (int i = 0; i < len; i++)
+                {
+                    w.Write(this.data[readIndex]);
+                    this.data[this.index] = this.data[readIndex];
+                    readIndex = (readIndex + 1) & this.mask;
+                    this.index = (this.index + 1) & this.mask;
+                }
+            }
+            else
+            {
+                int readIndex = (this.index - dist + this.data.Length) % this.data.Length;
+                for (int i = 0; i < len; i++)
+                {
+                    w.Write(this.data[readIndex]);
+                    this.data[this.index] = this.data[readIndex];
+                    readIndex = (readIndex + 1) % this.data.Length;
+                    this.index = (this.index + 1) % this.data.Length;
+                }
+            }
+        }
+    }
+}
