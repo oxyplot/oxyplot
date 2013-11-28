7d3771b7b4f3c04a20099c489acc6d5db727546b
objo <objo@localhost>
2013-11-28 20:16:59 +0100

    LinearColorAxis: set default Palette OxyPalette: add Reverse() method OxyPalettes: add HueDistinct palette New examples under "LinearColorAxis" showing the different palettes

--- Source/Examples/ExampleLibrary/Examples/HeatMapSeriesExamples.cs
+++ Source/Examples/ExampleLibrary/Examples/HeatMapSeriesExamples.cs
@@ -1,9 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ContourSeriesExamples.cs" company="OxyPlot">
+// <copyright file="HeatMapSeriesExamples.cs" company="OxyPlot">
 //   The MIT License (MIT)
-//
+//   
 //   Copyright (c) 2012 Oystein Bjorke
-//
+//   
 //   Permission is hereby granted, free of charge, to any person obtaining a
 //   copy of this software and associated documentation files (the
 //   "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 //   distribute, sublicense, and/or sell copies of the Software, and to
 //   permit persons to whom the Software is furnished to do so, subject to
 //   the following conditions:
-//
+//   
 //   The above copyright notice and this permission notice shall be included
 //   in all copies or substantial portions of the Software.
-//
+//   
 //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -28,44 +28,53 @@
 namespace ExampleLibrary
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
 
     using OxyPlot;
     using OxyPlot.Axes;
     using OxyPlot.Series;
 
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
     [Examples("HeatMapSeries")]
     public class HeatMapSeriesExamples : ExamplesBase
     {
         [Example("Peaks")]
         public static PlotModel Peaks()
         {
+            return CreatePeaks();
+        }
+
+        public static PlotModel CreatePeaks(OxyPalette palette = null, bool includeContours = true)
+        {
             double x0 = -3.1;
             double x1 = 3.1;
             double y0 = -3;
             double y1 = 3;
-            Func<double, double, double> peaks = (x, y) => 3 * (1 - x) * (1 - x) * Math.Exp(-(x * x) - (y + 1) * (y + 1))
-               - 10 * (x / 5 - x * x * x - y * y * y * y * y) * Math.Exp(-x * x - y * y)
-               - 1.0 / 3 * Math.Exp(-(x + 1) * (x + 1) - y * y);
+            Func<double, double, double> peaks = (x, y) => 3 * (1 - x) * (1 - x) * Math.Exp(-(x * x) - (y + 1) * (y + 1)) - 10 * (x / 5 - x * x * x - y * y * y * y * y) * Math.Exp(-x * x - y * y) - 1.0 / 3 * Math.Exp(-(x + 1) * (x + 1) - y * y);
             var xvalues = ArrayHelper.CreateVector(x0, x1, 100);
             var yvalues = ArrayHelper.CreateVector(y0, y1, 100);
             var peaksData = ArrayHelper.Evaluate(peaks, xvalues, yvalues);
 
             var model = new PlotModel("Peaks");
-            model.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Jet(500), HighColor = OxyColors.Gray, LowColor = OxyColors.Black });
+            model.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = palette ?? OxyPalettes.Jet(500), HighColor = OxyColors.Gray, LowColor = OxyColors.Black });
 
             var hms = new HeatMapSeries { X0 = x0, X1 = x1, Y0 = y0, Y1 = y1, Data = peaksData };
             model.Series.Add(hms);
-            var cs = new ContourSeries
-                {
-                    Color = OxyColors.Black,
-                    FontSize = 0,
-                    ContourLevelStep = 1,
-                    LabelBackground = OxyColors.Undefined,
-                    ColumnCoordinates = yvalues,
-                    RowCoordinates = xvalues,
-                    Data = peaksData
-                };
-            model.Series.Add(cs);
+            if (includeContours)
+            {
+                var cs = new ContourSeries
+                             {
+                                 Color = OxyColors.Black,
+                                 FontSize = 0,
+                                 ContourLevelStep = 1,
+                                 LabelBackground = OxyColors.Undefined,
+                                 ColumnCoordinates = yvalues,
+                                 RowCoordinates = xvalues,
+                                 Data = peaksData
+                             };
+                model.Series.Add(cs);
+            }
+
             return model;
         }
 
@@ -146,7 +155,7 @@ namespace ExampleLibrary
         }
 
         [Example("Diagonal (6x6)")]
-        public static PlotModel Diagonal_6x6()
+        public static PlotModel Diagonal_6X6()
         {
             // https://oxyplot.codeplex.com/discussions/453174
             var data = new double[6, 6];
