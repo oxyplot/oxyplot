d62e27d9ec300b1b706bf863a1b978043fc100ec
objo <oystein.bjorke@gmail.com>
2014-01-17 23:18:23 +0100

    Build script improvements (changing to new server based on Windows 8.1 and VS2013)

--- Build/UpdateExampleBrowser.cmd
+++
@@ -1,5 +0,0 @@
-echo off
-echo HOST: 'ftp.oxyplot.org'
-echo USER: '%OXYPLOT_FTP_USER%'
-echo PW:   '%OXYPLOT_FTP_PWD%'
-..\Tools\Lynx\FtpUpload.exe ftp.oxyplot.org %OXYPLOT_FTP_USER% %OXYPLOT_FTP_PWD% ..\Output\SL4\Examples\Silverlight\ExampleBrowser\ExampleBrowser.xap /oxyplot.org/wwwroot/ExampleBrowser/ExampleBrowser.xap
