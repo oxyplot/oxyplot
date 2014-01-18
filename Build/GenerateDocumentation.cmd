52d12a67ea8f0856fc79d39221bd9dba3cc51659
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-01-18 19:29:07 +0100

    Update version number in docs

--- Build/GenerateDocumentation.cmd
+++ Build/GenerateDocumentation.cmd
@@ -2,8 +2,8 @@
 mkdir ..\Documentation\Images\Series
 ..\Source\Examples\DocumentationExamples\ExampleGenerator\bin\Release\ExampleGenerator.exe ..\Documentation\Images\Series
 
-..\Tools\Lynx\WikiT.exe /format=html /template=..\Documentation\Web_Template.html /localLinks={0}.html /version=2013.1.1 /input=..\Documentation\*.wiki /output=..\Output\Documentation
-..\Tools\Lynx\WikiT.exe /format=html /template=..\Documentation\UserGuide_Template.html /version=2013.1.1 /localLinks={0}.html /input=..\Documentation\UserGuide.mwiki /output=..\Output\Documentation
+..\Tools\Lynx\WikiT.exe /format=html /template=..\Documentation\Web_Template.html /localLinks={0}.html /version=%1 /input=..\Documentation\*.wiki /output=..\Output\Documentation
+..\Tools\Lynx\WikiT.exe /format=html /template=..\Documentation\UserGuide_Template.html /version=%1 /localLinks={0}.html /input=..\Documentation\UserGuide.mwiki /output=..\Output\Documentation
 
 mkdir ..\Output\Documentation\Images
 copy ..\Documentation\*.css ..\Output\Documentation
