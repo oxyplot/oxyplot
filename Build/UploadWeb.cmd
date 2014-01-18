dbc19185557671da006f9fe8ea70d0b8d758cd6a
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-01-18 17:04:52 +0100

    Upload web page in build script

--- Build/UploadWeb.cmd
+++ Build/UploadWeb.cmd
@@ -1,4 +1,1 @@
-xcopy ..\icons\favicon.ico ..\Web /Y
-mkdir ..\Web\Examples
-xcopy ..\documentation\images\series ..\Web\Examples /Y
-..\Tools\Lynx\FtpUpload.exe ftp.oxyplot.org %OXYPLOT_FTP_USER% %OXYPLOT_FTP_PWD% ..\Web\*.* %OXYPLOT_FTP_FOLDER% /t=1
+..\Tools\Lynx\FtpUpload.exe ftp.oxyplot.org %OXYPLOT_FTP_USER% %OXYPLOT_FTP_PWD% ..\Web\*.* /oxyplot.org/wwwroot/ /t=1
