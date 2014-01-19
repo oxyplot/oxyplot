94d6436c106341cd37fe1fc0253d43f7bf723005
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-01-19 21:10:14 +0100

    ExampleLibrary: add "Discussion" examples, move open/closed issues to new folder

--- Source/Examples/ExampleLibrary/Issues/ClosedIssues.cs
+++ Source/Examples/ExampleLibrary/Issues/ClosedIssues.cs
@@ -41,6 +41,15 @@ namespace ExampleLibrary
     // ReSharper disable InconsistentNaming
     public class ClosedIssues : ExamplesBase
     {
+        [Example("#9971: Don't show minor ticks")]
+        public static PlotModel DontShowMinorTicks()
+        {
+            var model = new PlotModel("ShowMinorTicks = false");
+            model.Axes.Add(new LinearAxis(AxisPosition.Bottom) { ShowMinorTicks = false });
+            model.Axes.Add(new LinearAxis(AxisPosition.Left) { ShowMinorTicks = false });
+            return model;
+        }
+
         [Example("#9990: Major grid lines in front of minor")]
         public static PlotModel GridLinesBothDifferentColors()
         {
@@ -69,69 +78,6 @@ namespace ExampleLibrary
                     MinorGridlineThickness = 4,
                 };
             plotModel1.Axes.Add(bottomAxis);
-            return plotModel1;
-        }
-
-        [Example("D445576: Invisible contour series")]
-        public static PlotModel InvisibleContourSeries()
-        {
-            var model = new PlotModel("Invisible contour series");
-            var cs = new ContourSeries
-            {
-                IsVisible = false,
-                ColumnCoordinates = ArrayHelper.CreateVector(-1, 1, 0.05),
-                RowCoordinates = ArrayHelper.CreateVector(-1, 1, 0.05)
-            };
-            cs.Data = ArrayHelper.Evaluate((x, y) => x + y, cs.ColumnCoordinates, cs.RowCoordinates);
-            model.Series.Add(cs);
-            return model;
-        }
-
-        [Example("#10061: Argument out of range in OxyPlot mouse over")]
-        public static PlotModel ArgumentOutOfRangeInMouseOver()
-        {
-            var model = new PlotModel("Argument out of range in OxyPlot mouse over");
-            var ls = new LineSeries();
-            ls.Points.Add(new DataPoint(10, 10));
-            ls.Points.Add(new DataPoint(10, 10));
-            ls.Points.Add(new DataPoint(12, 10));
-            model.Series.Add(ls);
-            return model;
-        }
-
-        [Example("#9971: Don't show minor ticks")]
-        public static PlotModel DontShowMinorTicks()
-        {
-            var model = new PlotModel("ShowMinorTicks = false");
-            model.Axes.Add(new LinearAxis(AxisPosition.Bottom) { ShowMinorTicks = false });
-            model.Axes.Add(new LinearAxis(AxisPosition.Left) { ShowMinorTicks = false });
-            return model;
-        }
-
-        [Example("#100079: AreaSeries draws on top of other elements")]
-        public static PlotModel DefaultAnnotationLayer()
-        {
-            var plotModel1 = new PlotModel("Annotations should be drawn on top by default", "The line annotation should be on top!");
-            var areaSeries1 = new AreaSeries();
-            areaSeries1.Points.Add(new DataPoint(0, 50));
-            areaSeries1.Points.Add(new DataPoint(10, 40));
-            areaSeries1.Points.Add(new DataPoint(20, 60));
-            areaSeries1.Points2.Add(new DataPoint(0, 60));
-            areaSeries1.Points2.Add(new DataPoint(5, 80));
-            areaSeries1.Points2.Add(new DataPoint(20, 70));
-            areaSeries1.Color = OxyColors.Red;
-            areaSeries1.Color2 = OxyColors.Blue;
-            areaSeries1.Fill = OxyColors.Yellow;
-
-            plotModel1.Series.Add(areaSeries1);
-            var lineAnnotation = new LineAnnotation
-            {
-                Type = LineAnnotationType.Vertical,
-                Layer = AnnotationLayer.AboveSeries,
-                X = 6
-            };
-
-            plotModel1.Annotations.Add(lineAnnotation);
             return plotModel1;
         }
 
@@ -176,14 +122,26 @@ namespace ExampleLibrary
             return model;
         }
 
+        [Example("#10061: Argument out of range in OxyPlot mouse over")]
+        public static PlotModel ArgumentOutOfRangeInMouseOver()
+        {
+            var model = new PlotModel("Argument out of range in OxyPlot mouse over");
+            var ls = new LineSeries();
+            ls.Points.Add(new DataPoint(10, 10));
+            ls.Points.Add(new DataPoint(10, 10));
+            ls.Points.Add(new DataPoint(12, 10));
+            model.Series.Add(ls);
+            return model;
+        }
+
         [Example("#10076: Slow redraws with noisy data")]
-        public static PlotModel NoisyData(int n = 500)
+        public static PlotModel NoisyData()
         {
             var model = new PlotModel("Noisy data");
 
-            var points = new List<IDataPoint>(n);
+            var points = new List<IDataPoint>();
             var rng = new Random();
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < 500; i++)
             {
                 points.Add(new DataPoint(i + 1, rng.NextDouble()));
             }
@@ -225,6 +183,33 @@ namespace ExampleLibrary
             return model;
         }
 
+        [Example("#10079: AreaSeries draws on top of other elements")]
+        public static PlotModel DefaultAnnotationLayer()
+        {
+            var plotModel1 = new PlotModel("Annotations should be drawn on top by default", "The line annotation should be on top!");
+            var areaSeries1 = new AreaSeries();
+            areaSeries1.Points.Add(new DataPoint(0, 50));
+            areaSeries1.Points.Add(new DataPoint(10, 40));
+            areaSeries1.Points.Add(new DataPoint(20, 60));
+            areaSeries1.Points2.Add(new DataPoint(0, 60));
+            areaSeries1.Points2.Add(new DataPoint(5, 80));
+            areaSeries1.Points2.Add(new DataPoint(20, 70));
+            areaSeries1.Color = OxyColors.Red;
+            areaSeries1.Color2 = OxyColors.Blue;
+            areaSeries1.Fill = OxyColors.Yellow;
+
+            plotModel1.Series.Add(areaSeries1);
+            var lineAnnotation = new LineAnnotation
+            {
+                Type = LineAnnotationType.Vertical,
+                Layer = AnnotationLayer.AboveSeries,
+                X = 6
+            };
+
+            plotModel1.Annotations.Add(lineAnnotation);
+            return plotModel1;
+        }
+
         [Example("#10084: AreaSeries should respect CanTrackerInterpolatePoints")]
         public static PlotModel AreaSeries_CanTrackerInterpolatePointsFalse()
         {
@@ -252,14 +237,6 @@ namespace ExampleLibrary
             areaSeries1.Points2.Add(new DataPoint(5, 80));
             areaSeries1.Points2.Add(new DataPoint(20, 70));
             plotModel1.Series.Add(areaSeries1);
-            return plotModel1;
-        }
-
-        [Example("D461507: StairStepSeries NullReferenceException")]
-        public static PlotModel StairStepSeries_NullReferenceException()
-        {
-            var plotModel1 = new PlotModel("StairStepSeries NullReferenceException");
-            plotModel1.Series.Add(new StairStepSeries());
             return plotModel1;
         }
 
