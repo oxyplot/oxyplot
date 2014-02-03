dec0cb6581706e149c46b484bc5f09f5b643b587
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-02-03 21:48:04 +0100

    #9625 Add input enumerations and gestures

--- Source/OxyPlot/Input/Gestures/OxyMouseWheelGesture.cs
+++ Source/OxyPlot/Input/Gestures/OxyMouseWheelGesture.cs
@@ -1,9 +1,9 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="OxyMouseButton.cs" company="OxyPlot">
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OxyMouseWheelGesture.cs" company="OxyPlot">
 //   The MIT License (MIT)
-//
+//   
 //   Copyright (c) 2012 Oystein Bjorke
-//
+//   
 //   Permission is hereby granted, free of charge, to any person obtaining a
 //   copy of this software and associated documentation files (the
 //   "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 //   distribute, sublicense, and/or sell copies of the Software, and to
 //   permit persons to whom the Software is furnished to do so, subject to
 //   the following conditions:
-//
+//   
 //   The above copyright notice and this permission notice shall be included
 //   in all copies or substantial portions of the Software.
-//
+//   
 //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -24,44 +24,33 @@
 //   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 // <summary>
-//   The mouse buttons.
+//   Defines a mouse wheel gesture that can be used to invoke a command.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace OxyPlot
 {
     /// <summary>
-    /// Specifies constants that define which mouse button was pressed.
+    /// Defines a mouse wheel gesture that can be used to invoke a command.
     /// </summary>
-    public enum OxyMouseButton
+    public class OxyMouseWheelGesture : OxyInputGesture
     {
         /// <summary>
-        /// No mouse button.
+        /// Gets or sets the modifier keys.
         /// </summary>
-        None = 0,
+        public OxyModifierKeys Modifiers { get; set; }
 
         /// <summary>
-        /// The left mouse button.
+        /// Indicates whether the current object is equal to another object of the same type.
         /// </summary>
-        Left = 1,
-
-        /// <summary>
-        /// The middle mouse button.
-        /// </summary>
-        Middle = 2,
-
-        /// <summary>
-        /// The right mouse button.
-        /// </summary>
-        Right = 3,
-
-        /// <summary>
-        /// The first extended mouse button.
-        /// </summary>
-        XButton1 = 4,
-
-        /// <summary>
-        /// The second extended mouse button.
-        /// </summary>
-        XButton2 = 5,
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.
+        /// </returns>
+        public override bool Equals(OxyInputGesture other)
+        {
+            var mwg = other as OxyMouseGesture;
+            return mwg != null && mwg.Modifiers == this.Modifiers;
+        }
     }
 }
