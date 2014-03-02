f840f2d7eee9de79f4fcafd14ee88b42de198253
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-03-02 22:28:04 +0100

    iOS code cleanup

--- Source/OxyPlot.XamarinIOS/ExportExtensions.cs
+++ Source/OxyPlot.XamarinIOS/ExportExtensions.cs
@@ -27,37 +27,37 @@
 
 namespace OxyPlot.XamarinIOS
 {
-  using System.Drawing;
-  
-  using MonoTouch.Foundation;
-  using MonoTouch.UIKit;
+    using System.Drawing;
 
-	public static class ExportExtensions
-	{
-		public static NSData ToPng(this UIView view, RectangleF rect)
-		{
-			return view.GetImage(rect).AsPNG();
-		}
+    using MonoTouch.Foundation;
+    using MonoTouch.UIKit;
 
-		public static UIImage GetImage(this UIView view, RectangleF rect)
-		{
-			UIGraphics.BeginImageContext(rect.Size);
-			view.Draw(rect);
-			var image = UIGraphics.GetImageFromCurrentImageContext();
-			UIGraphics.EndImageContext();
+    public static class ExportExtensions
+    {
+        public static NSData ToPng(this UIView view, RectangleF rect)
+        {
+            return view.GetImage(rect).AsPNG();
+        }
 
-			return image;
-		}
+        public static UIImage GetImage(this UIView view, RectangleF rect)
+        {
+            UIGraphics.BeginImageContext(rect.Size);
+            view.Draw(rect);
+            var image = UIGraphics.GetImageFromCurrentImageContext();
+            UIGraphics.EndImageContext();
 
-		public static NSData ToPdf(this UIView view, RectangleF rect)
-		{
-			var data = new NSMutableData();
-			UIGraphics.BeginPDFContext(data, rect, null);
-			UIGraphics.BeginPDFPage();
-			view.Draw(rect);
-			UIGraphics.EndPDFContent();
+            return image;
+        }
 
-			return data;
-		}
-	}
+        public static NSData ToPdf(this UIView view, RectangleF rect)
+        {
+            var data = new NSMutableData();
+            UIGraphics.BeginPDFContext(data, rect, null);
+            UIGraphics.BeginPDFPage();
+            view.Draw(rect);
+            UIGraphics.EndPDFContent();
+
+            return data;
+        }
+    }
 }
