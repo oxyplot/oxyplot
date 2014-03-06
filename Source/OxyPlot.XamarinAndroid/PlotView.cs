41ca63a5905c3b73a835cef8c5d9ac31d99c18ee
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-03-06 20:38:38 +0100

    Android: support images, line joins, line dash style, include internet permission for example browser

--- Source/OxyPlot.XamarinAndroid/PlotView.cs
+++ Source/OxyPlot.XamarinAndroid/PlotView.cs
@@ -1,9 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="PlotView.cs" company="OxyPlot">
 //   The MIT License (MIT)
-//
+//   
 //   Copyright (c) 2012 Oystein Bjorke
-//
+//   
 //   Permission is hereby granted, free of charge, to any person obtaining a
 //   copy of this software and associated documentation files (the
 //   "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 //   distribute, sublicense, and/or sell copies of the Software, and to
 //   permit persons to whom the Software is furnished to do so, subject to
 //   the following conditions:
-//
+//   
 //   The above copyright notice and this permission notice shall be included
 //   in all copies or substantial portions of the Software.
-//
+//   
 //   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 //   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -23,140 +23,220 @@
 //   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
+// <summary>
+//   Represents a view that can show a PlotModel.
+// </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace OxyPlot.XamarinAndroid
 {
-    using Android.Content;
-    using Android.Graphics;
-    using Android.Util;
-    using Android.Views;
+	using Android.Content;
+	using Android.Graphics;
+	using Android.Util;
+	using Android.Views;
 
-    using OxyPlot;
+	using OxyPlot;
 
-    public class PlotView : View
-    {
-        private PlotModel model;
+	/// <summary>
+	/// Represents a view that can show a <see cref="PlotModel"/>.
+	/// </summary>
+	public class PlotView : View, IPlotControl
+	{
+		/// <summary>
+		/// The rendering lock object.
+		/// </summary>
+		private readonly object renderingLock = new object();
 
-        public PlotModel Model
-        {
-            get
-            {
-                return this.model;
-            }
-            set
-            {
-                if (this.model == value)
-                {
-                    return;
-                }
+		/// <summary>
+		/// The invalidation lock object.
+		/// </summary>
+		private readonly object invalidateLock = new object();
 
-                this.model = value;
-                this.OnModelChanged();
-            }
-        }
+		/// <summary>
+		/// The current model.
+		/// </summary>
+		private PlotModel model;
 
-        private void OnModelChanged()
-        {
-            this.InvalidatePlot(true);
-        }
+		/// <summary>
+		/// The current render context.
+		/// </summary>
+		private CanvasRenderContext rc;
 
-        private readonly object invalidateLock = new object();
-        private bool isModelInvalidated;
-        private bool updateDataFlag = true;
+		/// <summary>
+		/// The model invalidated flag.
+		/// </summary>
+		private bool isModelInvalidated;
 
-        public void InvalidatePlot(bool updateData)
-        {
-            lock (this.invalidateLock)
-            {
-                this.isModelInvalidated = true;
-                this.updateDataFlag = this.updateDataFlag || updateData;
-            }
+		/// <summary>
+		/// The update data flag.
+		/// </summary>
+		private bool updateDataFlag = true;
 
-            this.Invalidate();
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PlotView"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="attrs">The attribute set.</param>
+		public PlotView(Context context, IAttributeSet attrs) :
+		base(context, attrs)
+		{
+		}
 
-        public PlotView(Context context, IAttributeSet attrs) :
-            base(context, attrs)
-        {
-            this.Initialize();
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PlotView"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="attrs">The attribute set.</param>
+		/// <param name="defStyle">The definition style.</param>
+		public PlotView(Context context, IAttributeSet attrs, int defStyle) :
+		base(context, attrs, defStyle)
+		{
+		}
 
-        public PlotView(Context context, IAttributeSet attrs, int defStyle) :
-            base(context, attrs, defStyle)
-        {
-            this.Initialize();
-        }
+		/// <summary>
+		/// Gets or sets the plot model.
+		/// </summary>
+		/// <value>
+		/// The model.
+		/// </value>
+		public PlotModel Model
+		{
+			get
+			{
+				return this.model;
+			}
 
-        private CanvasRenderContext rc;
+			set
+			{
+				if (this.model != value)
+				{
+					this.model = value;
+					this.InvalidatePlot(true);
+				}
+			}
+		}
 
-        private void Initialize()
-        {
-            this.rc = new CanvasRenderContext();
-        }
+		/// <summary>
+		/// Gets the actual <see cref="PlotModel" /> of the control.
+		/// </summary>
+		public PlotModel ActualModel
+		{
+			get
+			{
+				return this.Model;
+			}
+		}
 
-        private readonly object renderingLock = new object();
+		/// <summary>
+		/// Hides the tracker.
+		/// </summary>
+		public void HideTracker()
+		{
+		}
 
-        protected override void OnDraw(Canvas canvas)
-        {
-            base.OnDraw(canvas);
-            canvas.DrawColor(Color.White);
+		/// <summary>
+		/// Hides the zoom rectangle.
+		/// </summary>
+		public void HideZoomRectangle()
+		{
+		}
 
-            lock (this.invalidateLock)
-            {
-                if (this.isModelInvalidated)
-                {
-                    if (this.model != null)
-                    {
-                        this.model.Update(this.updateDataFlag);
-                        this.updateDataFlag = false;
-                    }
+		/// <summary>
+		/// Invalidates the plot (not blocking the UI thread)
+		/// </summary>
+		/// <param name="updateData">if set to <c>true</c>, all data bindings will be updated.</param>
+		public void InvalidatePlot(bool updateData)
+		{
+			lock (this.invalidateLock)
+			{
+				this.isModelInvalidated = true;
+				this.updateDataFlag = this.updateDataFlag || updateData;
+			}
 
-                    this.isModelInvalidated = false;
-                }
-            }
+			this.Invalidate();
+		}
 
-            lock (this.renderingLock)
-            {
-                if (this.model != null)
-                {
-                    this.rc.SetTarget(canvas);
-                    using (var bounds = new Rect())
-                    {
-                        canvas.GetClipBounds(bounds);
+		/// <summary>
+		/// Refreshes the plot immediately (blocking UI thread)
+		/// </summary>
+		/// <param name="updateData">if set to <c>true</c>, all data collections will be updated.</param>
+		public void RefreshPlot(bool updateData)
+		{
+			this.InvalidatePlot(updateData);
+		}
 
-                        // Render the background
-                        if (!this.model.Background.IsUndefined())
-                        {
-                            // TODO: can we set this.Background instead?
-                            rc.DrawRectangle(new OxyRect(bounds.Left, bounds.Top, bounds.Width(), bounds.Height()), this.model.Background, OxyColors.Undefined, 0);
-                        }
-                        
-                        this.model.Render(rc, bounds.Right - bounds.Left, bounds.Bottom - bounds.Top);
-                    }
-                }
-            }
+		/// <summary>
+		/// Sets the cursor type.
+		/// </summary>
+		/// <param name="cursorType">The cursor type.</param>
+		public void SetCursorType(CursorType cursorType)
+		{
+		}
 
-            /*
-            canvas.DrawColor(Color.White);
+		/// <summary>
+		/// Shows the tracker.
+		/// </summary>
+		/// <param name="trackerHitResult">The tracker data.</param>
+		public void ShowTracker(TrackerHitResult trackerHitResult)
+		{
+		}
 
-            using (var paint = new Paint())
-            {
-                paint.AntiAlias = true;
-                paint.Color = new Color(0, 0, 0, 220);
-                paint.StrokeWidth = 2;
-                canvas.DrawLine(10f, 10f, canvas.Width - 10, h - 10, paint);
-                canvas.DrawLine(canvas.Width - 10, 10f, 10, h - 10, paint);
-                paint.TextSize = 24;
-                paint.TextAlign = Paint.Align.Left;
-                var textbounds = new Rect();
-                paint.GetTextBounds("ABC", 0, 1, textbounds);
-                var tw = paint.MeasureText("ABC");
-                // canvas.DrawText(this.Text, 40, 200, paint);
-                canvas.Translate(40, 40);
-                canvas.DrawText("ABC=" + tw + "/" + textbounds, 0, 0, paint);
-                canvas.Rotate(20);
-                canvas.DrawText("ABC=" + tw, 0, 0, paint);
-            }*/
-        }
-    }
+		/// <summary>
+		/// Shows the zoom rectangle.
+		/// </summary>
+		/// <param name="rectangle">The rectangle.</param>
+		public void ShowZoomRectangle(OxyRect rectangle)
+		{
+		}
+
+		/// <summary>
+		/// Stores text on the clipboard.
+		/// </summary>
+		/// <param name="text">The text.</param>
+		public void SetClipboardText(string text)
+		{
+		}
+
+		/// <summary>
+		/// Draws the content of the control.
+		/// </summary>
+		/// <param name="canvas">The canvas to draw on.</param>
+		protected override void OnDraw(Canvas canvas)
+		{
+			base.OnDraw(canvas);
+			var actualModel = this.ActualModel;
+			if (actualModel == null)
+			{
+				return;
+			}
+
+			var background = actualModel.Background.IsVisible () ? actualModel.Background : OxyColors.White;
+			canvas.DrawColor(background.ToColor());
+
+			lock (this.invalidateLock)
+			{
+				if (this.isModelInvalidated)
+				{
+					actualModel.Update(this.updateDataFlag);
+					this.updateDataFlag = false;
+					this.isModelInvalidated = false;
+				}
+			}
+
+			lock (this.renderingLock)
+			{
+				if (this.rc == null)
+				{
+					this.rc = new CanvasRenderContext();
+				}
+
+				this.rc.SetTarget(canvas);
+				using (var bounds = new Rect())
+				{
+					canvas.GetClipBounds(bounds);
+					actualModel.Render(this.rc, bounds.Right - bounds.Left, bounds.Bottom - bounds.Top);
+				}
+			}
+		}
+	}
 }
