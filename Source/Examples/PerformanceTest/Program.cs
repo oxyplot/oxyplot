eb2dd1f4b786aa3ce32cb1d129ba5279791f7b81
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-01 00:24:45 +0200

    Add PerformanceTest application (used for profiling)

---
+++ Source/Examples/PerformanceTest/Program.cs
@@ -0,0 +1,90 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="OxyPlot">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 OxyPlot contributors
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the
+//   "Software"), to deal in the Software without restriction, including
+//   without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and/or sell copies of the Software, and to
+//   permit persons to whom the Software is furnished to do so, subject to
+//   the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   The performance test main program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PerformanceTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    using OxyPlot;
+
+    /// <summary>
+    /// The performance test main program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The program entry point.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            TestDrawClippedLine(10000, 1000, false);
+            TestDrawClippedLine(10000, 1000, true);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Tests the <see cref="RenderingExtensions.DrawClippedLine" /> method.
+        /// </summary>
+        /// <param name="n">The number of points.</param>
+        /// <param name="m">The number of repetitions.</param>
+        /// <param name="useOutputBuffer"><c>true</c> to use an output buffer.</param>
+        public static void TestDrawClippedLine(int n, int m, bool useOutputBuffer)
+        {
+            var points = new ScreenPoint[n];
+            for (int i = 0; i < n; i++)
+            {
+                points[i] = new ScreenPoint((double)i / n, Math.Sin(40d * i / n));
+            }
+
+            var clippingRectangle = new OxyRect(0.3, -0.5, 0.5, 1);
+            var rc = new EmptyRenderContext();
+            IList<ScreenPoint> outputBuffer = useOutputBuffer ? new List<ScreenPoint>(n) : null;
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < m; i++)
+            {
+                rc.DrawClippedLine(
+                    points,
+                    clippingRectangle,
+                    1,
+                    OxyColors.Black,
+                    1,
+                    null,
+                    OxyPenLineJoin.Miter,
+                    false,
+                    outputBuffer);
+            }
+
+            Console.WriteLine((double)stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
