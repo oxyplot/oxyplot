6a721b329ec229c49a422e7fbdde57a9f28da480
Xavier <Xavier@xavier-PC.lsi>
2014-03-31 09:56:00 +0200

    Added the example for the ScatterWithErrorBar to the example library.

--- Source/Examples/ExampleLibrary/Examples/ScatterWithErrorBarSeriesExamples.cs
+++ Source/Examples/ExampleLibrary/Examples/ScatterWithErrorBarSeriesExamples.cs
@@ -1,14 +1,39 @@
 ﻿// <copyright file="ScatterWithErrorBarSeriesExamples.cs" company="LS Instruments">
 //     Copyright (c) 2014 LS Instruments. All rights reserved.
 // </copyright>
-namespace ExampleLibrary.Examples.BarSeries
+namespace ExampleLibrary
 {
+    using OxyPlot;
+    using OxyPlot.Series;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
-    public class ScatterWithErrorBarSeriesExamples
+    [Examples("ScatterWithErrorBarSeries")]
+    public class ScatterWithErrorBarSeriesExamples : ExamplesBase
     {
+        [Example("Random points and error")]
+        public static PlotModel ScatterWithErrorBarSeriesExample()
+        {
+            const int n = 20;
+            var model = new PlotModel(string.Format("Random data (n={0})", n)) { LegendPosition = LegendPosition.LeftTop };
+
+            var s1 = new ScatterWithErrorBarSeries { Title = "Measurements" };
+            var random = new Random();
+            double x = 0;
+            double y = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x += 2 + random.NextDouble() * 10;
+                y += 1 + random.NextDouble();
+                double error = random.NextDouble();
+
+                var p = new ScatterErrorPoint(x, y, error);
+                s1.Points.Add(p);
+            }
+            model.Series.Add(s1);
+            return model;
+        }
     }
 }
