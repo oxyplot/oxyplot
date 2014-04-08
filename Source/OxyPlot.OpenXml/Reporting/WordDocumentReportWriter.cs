31f629044f767c90f66233a76c5ee93e9e665971
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-08 20:33:40 +0200

    Correct wrong use of <seealso> XML comment

--- Source/OxyPlot.OpenXml/Reporting/WordDocumentReportWriter.cs
+++ Source/OxyPlot.OpenXml/Reporting/WordDocumentReportWriter.cs
@@ -80,10 +80,11 @@ namespace OxyPlot.OpenXml
     /// <summary>
     /// Provides a report writer for Word/OpenXML output using OpenXML SDK 2.0.
     /// </summary>
-    /// <seealso cref="http://www.codeproject.com/KB/office/OpenXML-SDK-HelloWorld.aspx" />
-    /// <seealso cref="http://msdn.microsoft.com/en-us/library/documentformat.openxml.wordprocessing.aspx" />
     public class WordDocumentReportWriter : IDisposable, IReportWriter
     {
+        //// http://www.codeproject.com/KB/office/OpenXML-SDK-HelloWorld.aspx
+        //// http://msdn.microsoft.com/en-us/library/documentformat.openxml.wordprocessing.aspx
+
         /// <summary>
         /// The body text id.
         /// </summary>
