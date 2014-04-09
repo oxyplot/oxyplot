51a276c88da54405637a0e7cfe8329cb2ae1ac89
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-09 23:40:43 +0200

    Some minor refactorings

--- Source/OxyPlot/Utilities/ReflectionHelper.cs
+++ Source/OxyPlot/Utilities/ReflectionHelper.cs
@@ -36,19 +36,22 @@ namespace OxyPlot
     using System.Globalization;
     using System.Reflection;
 
+    using OxyPlot.Axes;
+
     /// <summary>
     /// Provides utility methods reflection based support methods.
     /// </summary>
     public static class ReflectionHelper
     {
         /// <summary>
-        /// Fills a list by the specified property of a source list/enumerable.
+        /// Fills a target by the specified property of a source target/enumerable.
         /// </summary>
-        /// <typeparam name="T">The type of the destination list items (and the source property).</typeparam>
-        /// <param name="source">The source list.</param>
+        /// <typeparam name="T">The type of the destination target items (and the source property).</typeparam>
+        /// <param name="target">The target list to be filled.</param>
+        /// <param name="source">The source target.</param>
         /// <param name="propertyName">The property name.</param>
-        /// <param name="list">The list to be filled.</param>
-        public static void FillList<T>(IEnumerable source, string propertyName, IList<T> list)
+        /// <exception cref="System.InvalidOperationException">Could not find property.</exception>
+        public static void FillList<T>(List<T> target, IEnumerable source, string propertyName)
         {
             PropertyInfo pi = null;
             Type t = null;
@@ -60,14 +63,54 @@ namespace OxyPlot
                     pi = t.GetProperty(propertyName);
                     if (pi == null)
                     {
-                        throw new InvalidOperationException(
-                            string.Format("Could not find field {0} on type {1}", propertyName, t));
+                        throw new InvalidOperationException(string.Format("Could not find property {0} on type {1}", propertyName, t));
                     }
                 }
 
                 var v = pi.GetValue(o, null);
                 var value = (T)Convert.ChangeType(v, typeof(T), CultureInfo.InvariantCulture);
-                list.Add(value);
+                target.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Adds data points from the specified source to the specified destination.
+        /// </summary>
+        /// <param name="target">The destination target.</param>
+        /// <param name="itemsSource">The source.</param>
+        /// <param name="dataFieldX">The x-coordinate data field.</param>
+        /// <param name="dataFieldY">The y-coordinate data field.</param>
+        public static void FillList(List<DataPoint> target, IEnumerable itemsSource, string dataFieldX, string dataFieldY)
+        {
+            PropertyInfo pix = null;
+            PropertyInfo piy = null;
+            Type t = null;
+
+            foreach (var o in itemsSource)
+            {
+                if (pix == null || o.GetType() != t)
+                {
+                    t = o.GetType();
+                    pix = t.GetProperty(dataFieldX);
+                    piy = t.GetProperty(dataFieldY);
+                    if (pix == null)
+                    {
+                        throw new InvalidOperationException(
+                            string.Format("Could not find data field {0} on type {1}", dataFieldX, t));
+                    }
+
+                    if (piy == null)
+                    {
+                        throw new InvalidOperationException(
+                            string.Format("Could not find data field {0} on type {1}", dataFieldY, t));
+                    }
+                }
+
+                double x = Axis.ToDouble(pix.GetValue(o, null));
+                double y = Axis.ToDouble(piy.GetValue(o, null));
+
+                var pp = new DataPoint(x, y);
+                target.Add(pp);
             }
         }
     }
