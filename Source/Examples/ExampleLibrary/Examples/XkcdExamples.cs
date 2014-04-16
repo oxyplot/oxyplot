7a928f6b11238cf9ac9e27896b064b59a67c629b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-16 23:30:25 +0200

    Add ColumnSeries to xkcd examples

--- Source/Examples/ExampleLibrary/Examples/XkcdExamples.cs
+++ Source/Examples/ExampleLibrary/Examples/XkcdExamples.cs
@@ -113,5 +113,48 @@ namespace ExampleLibrary
 
             return model;
         }
+
+
+        /// <summary>
+        /// Xkcd style example #3.
+        /// </summary>
+        /// <returns>A <see cref="PlotModel" />.</returns>
+        [Example("Test #3")]
+        public static PlotModel Test3()
+        {
+            var model = new PlotModel
+            {
+                Title = "Test #3",
+                LegendPlacement = LegendPlacement.Outside,
+                LegendPosition = LegendPosition.BottomCenter,
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendBorderThickness = 0,
+                RenderingDecorator = rc => new XkcdRenderingDecorator(rc)
+            };
+
+            var s1 = new ColumnSeries { Title = "Series 1", IsStacked = false, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s1.Items.Add(new ColumnItem { Value = 25 });
+            s1.Items.Add(new ColumnItem { Value = 137 });
+            s1.Items.Add(new ColumnItem { Value = 18 });
+            s1.Items.Add(new ColumnItem { Value = 40 });
+
+            var s2 = new ColumnSeries { Title = "Series 2", IsStacked = false, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s2.Items.Add(new ColumnItem { Value = 12 });
+            s2.Items.Add(new ColumnItem { Value = 14 });
+            s2.Items.Add(new ColumnItem { Value = 120 });
+            s2.Items.Add(new ColumnItem { Value = 26 });
+
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Bottom };
+            categoryAxis.Labels.Add("Category A");
+            categoryAxis.Labels.Add("Category B");
+            categoryAxis.Labels.Add("Category C");
+            categoryAxis.Labels.Add("Category D");
+            var valueAxis = new LinearAxis { Position = AxisPosition.Left, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+            model.Series.Add(s1);
+            model.Series.Add(s2);
+            model.Axes.Add(categoryAxis);
+            model.Axes.Add(valueAxis);
+            return model;
+        }
     }
 }
