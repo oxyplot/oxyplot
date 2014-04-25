da3e64b26226b0d62a91855f658748c905321c04
Bart?omiej Szypelow <bszypelow@users.noreply.github.com>
2014-04-25 22:44:41 +0200

    Fix assembly name and platform

--- Source/OxyPlot.WP8/Properties/AssemblyInfo.cs
+++ Source/OxyPlot.WP8/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyTitle("OxyPlot.Metro")]
+[assembly: AssemblyTitle("OxyPlot.WP8")]
 [assembly: AssemblyDescription("OxyPlot controls for Windows Phone Store apps")]
