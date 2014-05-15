7e61c4a4d4386a997ae14469e9df2747623a322b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-05-15 22:04:31 +0200

    Fix obsolete constructors in examples

--- Source/Examples/Silverlight/HistogramTest/OxyPlotTestHarness.cs
+++ Source/Examples/Silverlight/HistogramTest/OxyPlotTestHarness.cs
@@ -56,8 +56,8 @@ namespace Visiblox.Charts.Examples
         {
             var model = new PlotModel();
             model.PlotMargins = new OxyThickness(10, 10, 10, 20);
-            model.Axes.Add(new LinearAxis(AxisPosition.Left, 0, 250, 50, 100) { MajorGridlineStyle = LineStyle.Solid, TickStyle = TickStyle.None });
-            model.Axes.Add(new LinearAxis(AxisPosition.Bottom, 0) { MajorGridlineStyle = LineStyle.Solid, TickStyle = TickStyle.None });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Minimum = 0, Maximum = 250, MinorStep = 50, MajorStep = 100, MajorGridlineStyle = LineStyle.Solid, TickStyle = TickStyle.None });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = 0, MajorGridlineStyle = LineStyle.Solid, TickStyle = TickStyle.None });
             model.Series.Add(ListToSeries(rgbData[0], OxyColors.Red));
             model.Series.Add(ListToSeries(rgbData[1], OxyColors.Green));
             model.Series.Add(ListToSeries(rgbData[2], OxyColors.Blue));
