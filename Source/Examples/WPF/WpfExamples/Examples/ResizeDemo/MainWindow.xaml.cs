f9c72bdb3e2507511fb2eedf37d8b81078fc021d
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-05-28 22:19:44 +0200

    #10210: add resize demo

---
+++ Source/Examples/WPF/WpfExamples/Examples/ResizeDemo/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="OxyPlot">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 OxyPlot contributors
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the
+//   "Software"), to deal in the Software without restriction, including
+//   without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and/or sell copies of the Software, and to
+//   permit persons to whom the Software is furnished to do so, subject to
+//   the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ResizeDemo
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Axes;
+    using OxyPlot.Series;
+
+    using WpfExamples;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example("Demonstrates resizing of the PlotView with grid splitters.")]
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+
+            this.Model1 = CreateModel("Model 1");
+            this.Model2 = CreateModel("Model 2");
+            this.Model3 = CreateModel("Model 3");
+        }
+
+        /// <summary>
+        /// Gets the left model.
+        /// </summary>
+        public PlotModel Model1 { get; private set; }
+
+        /// <summary>
+        /// Gets the right-top model.
+        /// </summary>
+        public PlotModel Model2 { get; private set; }
+
+        /// <summary>
+        /// Gets the right-bottom model.
+        /// </summary>
+        public PlotModel Model3 { get; private set; }
+
+        /// <summary>
+        /// Creates a sample model.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <returns>A <see cref="PlotModel" />.</returns>
+        private static PlotModel CreateModel(string title)
+        {
+            var model = new PlotModel { Title = title };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            model.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 1000));
+            model.Series.Add(new FunctionSeries(x => Math.Sin(x) / x, 0, 10, 1000));
+            return model;
+        }
+    }
+}
