7cc3a2ed319a06c90f1797a7a0158647f62636b6
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-09-09 15:14:07 +0200

    Show informational version in Silverlight example browser

--- Source/Examples/Silverlight/ExampleBrowser/MainPage.xaml.cs
+++ Source/Examples/Silverlight/ExampleBrowser/MainPage.xaml.cs
@@ -4,25 +4,14 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-
 namespace ExampleBrowser
 {
-    public partial class MainPage : UserControl
+    public partial class MainPage
     {
         public MainPage()
         {
-            InitializeComponent();
-            DataContext = new MainWindowViewModel();
+            this.InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
-
 }
