aad9b6f3c6475f76f39dfadca541c07063a3e07b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-09-11 00:06:08 +0200

    Update Xamarin NuGet packages

--- Build/pack.sh
+++ Build/pack.sh
@@ -2,10 +2,27 @@
 
 NUGET=/usr/local/bin/NuGet.exe
 
+rm -f ../Output/*.nupkg
+
+cp ../README.md ../Output
+cp ../LICENSE ../Output
+cp ../AUTHORS ../Output
+cp ../CONTRIBUTORS ../Output
+cp ../README ../Output
+mkdir ../Output/lib
+mkdir ../Output/lib/MonoTouch
+mkdir ../Output/lib/MonoAndroid
+
+cp ../Output/XamarinIOS/OxyPlot.XamarinIOS.??? ../Output/lib/MonoTouch
+cp ../Output/XamarinAndroid/OxyPlot.XamarinAndroid.??? ../Output/lib/MonoAndroid
+
+cp ../Source/OxyPlot.XamarinIOS/*.nuspec ../Output
+cp ../Source/OxyPlot.XamarinAndroid/*.nuspec ../Output
+
 # Create XamarinIOS NuGet package
-mono $NUGET pack ../Source/OxyPlot.XamarinIOS/OxyPlot.XamarinIOS.nuspec -OutputDirectory ../Output > pack.log
+mono $NUGET pack ../Output/OxyPlot.XamarinIOS.nuspec -OutputDirectory ../Output
 
 # Create XamarinAndroid NuGet package
-mono $NUGET pack ../Source/OxyPlot.XamarinAndroid/OxyPlot.XamarinAndroid.nuspec -OutputDirectory ../Output >> pack.log
+mono $NUGET pack ../Source/OxyPlot.XamarinAndroid/OxyPlot.XamarinAndroid.nuspec -OutputDirectory ../Output
 
 ls -al ../Output/*.nupkg
