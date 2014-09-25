c217cf42c6f239c0c594b6c6e4eefffc9702ddc5
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-09-25 23:38:03 +0200

    Update nuspec

--- Build/Pack_XamarinForms.cmd
+++ Build/Pack_XamarinForms.cmd
@@ -2,8 +2,6 @@ del ../Output/*.nupkg
 
 cd ..\Source
 
-UpdateVersionNumbers.exe /VersionFromNuGet=OxyPlot.Core /Dependency=OxyPlot.Core /Directory=..
-
 nuget pack OxyPlot.XamarinForms/OxyPlot.XamarinForms.nuspec -OutputDirectory ../Output
 
 cd ..\Build
