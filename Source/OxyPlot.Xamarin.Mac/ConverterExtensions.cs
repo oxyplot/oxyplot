7e67a1080b4ea2cefd38039f6ab73320884bafff
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-10-16 21:54:05 +0200

    Improve rectangle conversion for Mac

--- Source/OxyPlot.Xamarin.Mac/ConverterExtensions.cs
+++ Source/OxyPlot.Xamarin.Mac/ConverterExtensions.cs
@@ -26,19 +26,19 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="p">The point to convert.</param>
         /// <returns>The converted point.</returns>
-        public static ScreenPoint ToScreenPoint(this CGPoint p)
+        public static ScreenPoint ToScreenPoint (this CGPoint p)
         {
-            return new ScreenPoint(p.X, p.Y);
+            return new ScreenPoint (p.X, p.Y);
         }
-            
+
         /// <summary>
         /// Converts a <see cref="System.Drawing.PointF" /> to a <see cref="ScreenPoint" />.
         /// </summary>
         /// <param name="p">The point to convert.</param>
         /// <returns>The converted point.</returns>
-        public static ScreenPoint LocationToScreenPoint(this CGPoint p, CGRect bounds)
+        public static ScreenPoint LocationToScreenPoint (this CGPoint p, CGRect bounds)
         {
-            return new ScreenPoint(p.X, bounds.Height - p.Y);
+            return new ScreenPoint (p.X, bounds.Height - p.Y);
         }
 
         /// <summary>
@@ -47,9 +47,9 @@ namespace OxyPlot.Xamarin.Mac
         /// <param name="c">The color to convert.</param>
         /// <returns>The converted color.</returns>
         // ReSharper disable once InconsistentNaming
-        public static CGColor ToCGColor(this OxyColor c)
+        public static CGColor ToCGColor (this OxyColor c)
         {
-            return new CGColor(c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f);
+            return new CGColor (c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f);
         }
 
         /// <summary>
@@ -57,10 +57,9 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="lineJoin">The line join.</param>
         /// <returns>The converted join.</returns>
-        public static CGLineJoin Convert(this LineJoin lineJoin)
+        public static CGLineJoin Convert (this LineJoin lineJoin)
         {
-            switch (lineJoin)
-            {
+            switch (lineJoin) {
             case LineJoin.Bevel:
                 return CGLineJoin.Bevel;
             case LineJoin.Miter:
@@ -68,7 +67,7 @@ namespace OxyPlot.Xamarin.Mac
             case LineJoin.Round:
                 return CGLineJoin.Round;
             default:
-                throw new InvalidOperationException("Invalid join type.");
+                throw new InvalidOperationException ("Invalid join type.");
             }
         }
 
@@ -77,9 +76,9 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="p">The point to convert.</param>
         /// <returns>The converted point.</returns>
-        public static CGPoint Convert(this ScreenPoint p)
+        public static CGPoint Convert (this ScreenPoint p)
         {
-            return new CGPoint((float)p.X, (float)p.Y);
+            return new CGPoint ((float)p.X, (float)p.Y);
         }
 
         /// <summary>
@@ -87,9 +86,9 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="p">The point to convert.</param>
         /// <returns>The converted point.</returns>
-        public static CGPoint ConvertAliased(this ScreenPoint p)
+        public static CGPoint ConvertAliased (this ScreenPoint p)
         {
-            return new CGPoint(0.5f + (int)p.X, 0.5f + (int)p.Y);
+            return new CGPoint (0.5f + (float)Math.Round (p.X), 0.5f + (float)Math.Round (p.Y));
         }
 
         /// <summary>
@@ -97,13 +96,13 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="rect">The rectangle to convert.</param>
         /// <returns>The converted rectangle.</returns>
-        public static CGRect ConvertAliased(this OxyRect rect)
+        public static CGRect ConvertAliased (this OxyRect rect)
         {
-            float x = 0.5f + (int)rect.Left;
-            float y = 0.5f + (int)rect.Top;
-            float ri = 0.5f + (int)rect.Right;
-            float bo = 0.5f + (int)rect.Bottom;
-            return new CGRect(x, y, ri - x, bo - y);
+            float x = 0.5f + (float)Math.Round (rect.Left);
+            float y = 0.5f + (float)Math.Round (rect.Top);
+            float w = 0.5f + (float)Math.Round (rect.Right) - x;
+            float h = 0.5f + (float)Math.Round (rect.Bottom) - y;
+            return new CGRect (x, y, w, h);
         }
 
         /// <summary>
@@ -111,16 +110,13 @@ namespace OxyPlot.Xamarin.Mac
         /// </summary>
         /// <param name="rect">The rectangle to convert.</param>
         /// <returns>The converted rectangle.</returns>
-        public static CGRect Convert(this OxyRect rect)
+        public static CGRect Convert (this OxyRect rect)
         {
-            var left = (int)rect.Left;
-            var right = (int)rect.Right;
-            var top = (int)rect.Top;
-            var bottom = (int)rect.Bottom;
-            return new CGRect(left, top, right - left, bottom - top);
+            return new CGRect ((float)rect.Left, (float)rect.Top, (float)(rect.Right - rect.Left), (float)(rect.Bottom - rect.Top));
         }
 
-        public static OxyMouseButton ToButton(this NSEventType theType){
+        public static OxyMouseButton ToButton (this NSEventType theType)
+        {
             switch (theType) {
             case NSEventType.LeftMouseDown:
                 return OxyMouseButton.Left;
@@ -133,55 +129,62 @@ namespace OxyPlot.Xamarin.Mac
             }
         }
 
-        public static OxyModifierKeys ToModifierKeys(this NSEventModifierMask theMask){
+        public static OxyModifierKeys ToModifierKeys (this NSEventModifierMask theMask)
+        {
             var keys = OxyModifierKeys.None;
             if ((theMask & NSEventModifierMask.ShiftKeyMask) == NSEventModifierMask.ShiftKeyMask)
                 keys |= OxyModifierKeys.Shift;
-                if ((theMask & NSEventModifierMask.ControlKeyMask) == NSEventModifierMask.ControlKeyMask)
+            if ((theMask & NSEventModifierMask.ControlKeyMask) == NSEventModifierMask.ControlKeyMask)
                 keys |= OxyModifierKeys.Control;
-                    if ((theMask & NSEventModifierMask.AlternateKeyMask )== NSEventModifierMask.AlternateKeyMask)
+            if ((theMask & NSEventModifierMask.AlternateKeyMask) == NSEventModifierMask.AlternateKeyMask)
                 keys |= OxyModifierKeys.Alt;
 
             // TODO
-                        if ((theMask & NSEventModifierMask.CommandKeyMask )== NSEventModifierMask.CommandKeyMask)
+            if ((theMask & NSEventModifierMask.CommandKeyMask) == NSEventModifierMask.CommandKeyMask)
                 keys |= OxyModifierKeys.Control;
 
             return keys;
         }
 
-        public static OxyMouseDownEventArgs ToMouseDownEventArgs(this NSEvent theEvent, CGRect bounds){
+        public static OxyMouseDownEventArgs ToMouseDownEventArgs (this NSEvent theEvent, CGRect bounds)
+        {
             // https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSEvent_Class/Reference/Reference.html
-            return new OxyMouseDownEventArgs{
-                Position=theEvent.LocationInWindow.LocationToScreenPoint(bounds),
-                ChangedButton=theEvent.Type.ToButton(),
-                ModifierKeys=theEvent.ModifierFlags.ToModifierKeys(),
-                ClickCount=(int)theEvent.ClickCount,
+            return new OxyMouseDownEventArgs {
+                Position = theEvent.LocationInWindow.LocationToScreenPoint (bounds),
+                ChangedButton = theEvent.Type.ToButton (),
+                ModifierKeys = theEvent.ModifierFlags.ToModifierKeys (),
+                ClickCount = (int)theEvent.ClickCount,
             };
         }
 
-        public static OxyMouseEventArgs ToMouseEventArgs(this NSEvent theEvent, CGRect bounds){
-            return new OxyMouseEventArgs{
-                Position=theEvent.LocationInWindow.LocationToScreenPoint(bounds),
-                ModifierKeys=theEvent.ModifierFlags.ToModifierKeys(),
+        public static OxyMouseEventArgs ToMouseEventArgs (this NSEvent theEvent, CGRect bounds)
+        {
+            return new OxyMouseEventArgs {
+                Position = theEvent.LocationInWindow.LocationToScreenPoint (bounds),
+                ModifierKeys = theEvent.ModifierFlags.ToModifierKeys (),
             };
         }
 
-        public static OxyMouseWheelEventArgs ToMouseWheelEventArgs(this NSEvent theEvent, CGRect bounds){
-            return new OxyMouseWheelEventArgs{
+        public static OxyMouseWheelEventArgs ToMouseWheelEventArgs (this NSEvent theEvent, CGRect bounds)
+        {
+            return new OxyMouseWheelEventArgs {
                 Delta = (int)theEvent.ScrollingDeltaY,
-                Position=theEvent.LocationInWindow.LocationToScreenPoint(bounds),
-                ModifierKeys=theEvent.ModifierFlags.ToModifierKeys(),
+                Position = theEvent.LocationInWindow.LocationToScreenPoint (bounds),
+                ModifierKeys = theEvent.ModifierFlags.ToModifierKeys (),
             };
         }
 
-        public static OxyKeyEventArgs ToKeyEventArgs(this NSEvent theEvent){
-            return new OxyKeyEventArgs{
-                Key = theEvent.KeyCode.ToKey(),
-                ModifierKeys=theEvent.ModifierFlags.ToModifierKeys(),
+        public static OxyKeyEventArgs ToKeyEventArgs (this NSEvent theEvent)
+        {
+            return new OxyKeyEventArgs {
+                Key = theEvent.KeyCode.ToKey (),
+                ModifierKeys = theEvent.ModifierFlags.ToModifierKeys (),
             };
         }
 
-        public static OxyKey ToKey(this ushort keycode){
+        public static OxyKey ToKey (this ushort keycode)
+        {
+            // TODO
             return OxyKey.A;
         }
     }
