af77210d0d685a50caec012520e14bc78581df6a
Thomas Ibel <tibel@users.noreply.github.com>
2014-11-07 16:12:40 +0100

    remove OxyPlot.Metro (#84)

--- Build/BuildRelease_Metro.cmd
+++
@@ -1,5 +0,0 @@
-@REM Requires MS Build Tools 2013
-@REM http://www.microsoft.com/en-us/download/details.aspx?id=40760
-@REM http://timrayburn.net/blog/visual-studio-2013-and-msbuild/
-
-"C:\Program Files (x86)\MSBuild\12.0\bin\amd64\msbuild.exe" ..\Source\OxyPlot.Metro.sln /p:Configuration=Release /m
