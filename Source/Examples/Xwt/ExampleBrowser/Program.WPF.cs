5ab73d6fc117f133fa583c30fdb84a543da46682
Vsevolod Kukol <sevo@sevo.org>
2014-12-08 23:58:03 +0100

    Add  Xwt backend

---
+++ Source/Examples/Xwt/ExampleBrowser/Program.WPF.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Xwt;
+using System;
+
+namespace ExampleBrowser
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			Application.Initialize (ToolkitType.Wpf);
+
+			MainWindow w = new MainWindow ();
+			w.Show ();
+
+			Application.Run ();
+
+			w.Dispose ();
+
+			Application.Dispose ();
+		}
+	}
+}
