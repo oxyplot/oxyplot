815941ce7444330b0f2e861c6f952f839cdea308
Vsevolod Kukol <v.kukol@rubologic.de>
2014-12-13 10:11:25 +0100

    Add Xwt.Mac configurations and examples

---
+++ Source/Examples/Xwt/ExampleBrowser/Program.Mac.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Xwt;
+
+namespace ExampleBrowser
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Initialize (ToolkitType.Gtk3);
+
+			MainWindow w = new MainWindow ();
+			w.Show ();
+
+			Application.Run ();
+
+			w.Dispose ();
+
+			Application.Dispose ();
+		}
+	}
+}
