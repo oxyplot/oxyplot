9e69129412afd3fb5b807035a7e9988794e9d822
piotr <pw@piootr.pl>
2015-01-05 20:26:54 +0100

    Added example with wpf wrapper for two-color area series in WpfExamles

---
+++ Source/Examples/WPF/WpfExamples/Examples/TwoColorAreaDemo/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainViewModel.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TwoColorAreaDemo
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.Measurements = new Collection<Measurement>();
+            var r = new Random(31);
+            double p1 = 0;
+            double p2 = 5;
+            double v1 = 0;
+            double v2 = 0;
+            for (int i = 0; i < 200; i++)
+            {
+                v1 += (r.NextDouble() - 0.5) * 0.7;
+                v2 += (r.NextDouble() - 0.5) * 0.1;
+                double y1 = p1 + v1;
+                double y2 = p2 + v2;
+                p1 = y1;
+                p2 = y2;
+                this.Measurements.Add(new Measurement
+                                     {
+                                         Time = i * 2.5,
+                                         Value = y1,
+                                         Maximum = y1 + y2,
+                                         Minimum = y1 - y2
+                                     });
+            }
+        }
+
+        public Collection<Measurement> Measurements { get; private set; }
+    }
+
+    public class Measurement
+    {
+        public double Time { get; set; }
+        public double Value { get; set; }
+        public double Minimum { get; set; }
+        public double Maximum { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0:#0.0} {1:##0.0}", Time, Value);
+        }
+    }
+}
---
+++ Source/Examples/WPF/WpfExamples/Examples/TwoColorAreaDemo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TwoColorAreaDemo
+{
+    using System.Windows;
+
+    using WpfExamples;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example(null, "Plotting with TwoColorAreaSeries.")]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
