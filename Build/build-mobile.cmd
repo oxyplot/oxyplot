329d18b2fb49c80659f9ae851d46db1e23ec5bb0
Matthew Leibowitz <mattleibow@live.com>
2015-02-15 22:30:32 +0200

    update the mobile command to build all mobile
     - OxyPlot.Mobile
     - OxyPlot.Xamarin.Forms
     - added the OxyPlot.Mobile dependency to the version numbers command
     - go back to initial dir after build

--- Build/build-mobile.cmd
+++ Build/build-mobile.cmd
@@ -3,8 +3,11 @@ git reset --hard HEAD
 
 del /S /Q Output\*.*
 
-UpdateVersionNumbers.exe /VersionFromNuGet=OxyPlot.Core /Dependency=OxyPlot.Core /ExtractReleaseNotes=CHANGELOG.md /Directory=.
+UpdateVersionNumbers.exe /VersionFromNuGet=OxyPlot.Core /Dependency=OxyPlot.Core /Dependency=OxyPlot.Mobile /ExtractReleaseNotes=CHANGELOG.md /Directory=.
 
 msbuild.exe Source\OxyPlot.Mobile.sln /p:Configuration=Release
 
 nuget pack Source\OxyPlot.Mobile.nuspec -OutputDirectory Output
+nuget pack Source\OxyPlot.Xamarin.Forms\OxyPlot.Xamarin.Forms.nuspec -OutputDirectory Output
+
+cd build
