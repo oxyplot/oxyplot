a34950ee67a9a68dfa0d6da42fdd1c6080ba55f6
Matthew Leibowitz <mattleibow@live.com>
2015-06-25 03:01:30 +0200

    Make sure the script fails if there was an error

--- Build/build-xamarin.sh
+++ Build/build-xamarin.sh
@@ -20,33 +20,27 @@ rm -rf $OUTPUT
 # Run the tool that updates the version numbers in all AssemblyInfo.cs files
 echo "Updating version numbers..."
 mono "$LynxToolkit/UpdateVersionNumbers.exe" /VersionFromNuGet=OxyPlot.Core /Dependency=OxyPlot.Core /ExtractReleaseNotes=../CHANGELOG.md /Directory=..
-if [ $? -ne 0 ]; then echo "  FAILED"; else echo "  OK"; fi
-
 
 # Start the build. The output will be created in the $OUTPUT folder.
 
 # MonoTouch
 echo "Building for MonoTouch..."
 "$MDTOOL" build "--configuration:Release" $SOURCE/OxyPlot.MonoTouch.sln > build-monotouch.log
-if [ $? -ne 0 ]; then echo "  FAILED"; else echo "  OK"; fi
 ls -al ../Output/MonoTouch/OxyPlot*
 
 # Xamarin.iOS
 echo "Building for Xamarin.iOS..."
 "$MDTOOL" build "--configuration:Release" $SOURCE/OxyPlot.Xamarin.iOS.sln > build-ios.log
-if [ $? -ne 0 ]; then echo "  FAILED"; else echo "  OK"; fi
 ls -al ../Output/Xamarin.iOS/OxyPlot*
 
 # Xamarin.Android
 echo "Building for Xamarin.Android..."
 "$MDTOOL" build "--configuration:Release" $SOURCE/OxyPlot.Xamarin.Android.sln > build-android.log
-if [ $? -ne 0 ]; then echo "  FAILED"; else echo "  OK"; fi
 ls -al ../Output/Xamarin.Android/OxyPlot*
 
 # Xamarin.Mac
 echo "Building for Xamarin.Mac..."
 "$MDTOOL" build "--configuration:Release" $SOURCE/OxyPlot.Xamarin.Mac.sln > build-mac.log
-if [ $? -ne 0 ]; then echo "  FAILED"; else echo "  OK"; fi
 ls -al ../Output/Xamarin.Mac/OxyPlot*
 
 
