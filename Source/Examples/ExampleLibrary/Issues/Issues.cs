3d211cf177292b597d6ad3095b557606cc455726
Alexei Shcherbakov <falcon.mk2@gmail.com>
2015-08-03 18:02:34 +0300

    Add Example for https://github.com/oxyplot/oxyplot/issues/42 (also https://github.com/oxyplot/oxyplot/issues/511)

--- Source/Examples/ExampleLibrary/Issues/Issues.cs
+++ Source/Examples/ExampleLibrary/Issues/Issues.cs
@@ -1160,6 +1160,42 @@ namespace ExampleLibrary
 			return model;
 		}
 
+		[Example("#42: ContourSeries not working for not square data array")]
+		public static PlotModel IssueDescription()
+		{
+			int n = 100;
+			double x0 = -3.1;
+			double x1 = 3.1;
+			double y0 = -3;
+			double y1 = 3;
+			Func<double, double, double> peaks = (x, y) => 3 * (1 - x) * (1 - x) * Math.Exp(-(x * x) - (y + 1) * (y + 1)) - 10 * (x / 5 - x * x * x - y * y * y * y * y) * Math.Exp(-x * x - y * y) - 1.0 / 3 * Math.Exp(-(x + 1) * (x + 1) - y * y);
+			// see https://github.com/oxyplot/oxyplot/issues/511
+			var xvalues = ArrayBuilder.CreateVector(x0, x1, n * 10);
+			var yvalues = ArrayBuilder.CreateVector(y0, y1, n);
+			var peaksData = ArrayBuilder.Evaluate(peaks, xvalues, yvalues);
+			var model = new PlotModel { Title = "Peaks" };
+			model.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Jet(500), HighColor = OxyColors.Gray, LowColor = OxyColors.Black });
+
+			var hms = new HeatMapSeries { X0 = x0, X1 = x1, Y0 = y0, Y1 = y1, Data = peaksData };
+			model.Series.Add(hms);
+			//if (includeContours)
+			{
+				var cs = new ContourSeries
+				{
+					Color = OxyColors.Black,
+					FontSize = 0,
+					ContourLevelStep = 1,
+					LabelBackground = OxyColors.Undefined,
+					ColumnCoordinates = yvalues,
+					RowCoordinates = xvalues,
+					Data = peaksData
+				};
+				model.Series.Add(cs);
+			}
+
+			return model;
+		}
+
         /* NEW ISSUE TEMPLATE
         [Example("#123: Issue Description")]
         public static PlotModel IssueDescription()
