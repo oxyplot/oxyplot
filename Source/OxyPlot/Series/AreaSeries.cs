ade519a672c88141dbe8db73ab6c0860a5c1719c
Paul Kertscher <p.k@pajap.eu>
2015-11-26 13:40:07 +0100

    Update AreaSeries.cs
    Fixed missing parenthesis

--- Source/OxyPlot/Series/AreaSeries.cs
+++ Source/OxyPlot/Series/AreaSeries.cs
@@ -120,7 +120,7 @@ namespace OxyPlot.Series
             {
                 return this.ItemsSource != null 
                     ? this.itemsSourcePoints2 
-                    : (this.points2.Any() ? this.points2 : this.Points.Select(p => new DataPoint(p.X, ConstantY2));
+                    : (this.points2.Any() ? this.points2 : this.Points.Select(p => new DataPoint(p.X, ConstantY2)));
             }
         }
 
