278bae1dc0c50ac5347b8e4f35d95a8d6c811611
Oystein Bjorke <oystein.bjorke@gmail.com>
2015-12-14 15:25:11 +0100

    Add LegendMaxHeight property to Wpf.Plot (#668)

--- Source/Examples/WPF/WpfExamples/Observable.cs
+++ Source/Examples/WPF/WpfExamples/Observable.cs
@@ -4,45 +4,9 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.ComponentModel;
-using System.Linq.Expressions;
-using System.Reflection;
-
 namespace WpfExamples
 {
-    public class Observable : INotifyPropertyChanged
+    public class Observable : PropertyTools.Observable
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChanged(string property)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(property));
-            }
-        }
-
-        protected void RaisePropertyChanged(Expression<Func<object>> expression)
-        {
-            var lambda = expression as LambdaExpression;
-            MemberExpression memberExpression;
-            if (lambda.Body is UnaryExpression)
-            {
-                var unaryExpression = lambda.Body as UnaryExpression;
-                memberExpression = unaryExpression.Operand as MemberExpression;
-            }
-            else
-            {
-                memberExpression = lambda.Body as MemberExpression;
-            }
-            if (memberExpression != null)
-            {
-                var propertyInfo = memberExpression.Member as PropertyInfo;
-                if (propertyInfo != null)
-                    RaisePropertyChanged(propertyInfo.Name);
-            }
-        }
     }
 }
