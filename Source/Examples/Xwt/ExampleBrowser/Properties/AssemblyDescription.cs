407d18f8d81c67480d4c6846078e3f8e74695cc9
Oystein Bjorke <oystein.bjorke@dnvgl.com>
2016-02-15 20:52:25 +0100

    Move OxyPlot.Xwt to its own repository #778

--- Source/Examples/Xwt/ExampleBrowser/Properties/AssemblyDescription.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("OxyPlot Xwt demo - Example browser")]
-[assembly: AssemblyDescription("Xwt based browser for the example library.")]
--- Source/OxyPlot.Xwt/Properties/AssemblyDescription.cs
+++
@@ -1,9 +0,0 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyDescription.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-using System.Reflection;
-
-[assembly: AssemblyTitle("OxyPlot for Xwt")]
-[assembly: AssemblyDescription("OxyPlot widget for Xwt")]
