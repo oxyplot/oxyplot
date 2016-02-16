1fde06cac27410e47bdb370188fe8bdf96399086
icerocker <henningbjorgo@gmail.com>
2016-02-16 22:19:41 +0100

    Added demo project that crashes.

---
+++ Source/Examples/WPF/PlotModelIssue/Item.cs
@@ -0,0 +1,22 @@
+﻿using OxyPlot;
+
+namespace PlotModelIssue
+{
+    public class Item
+    {
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        private PlotModel model = new PlotModel();
+
+        public PlotModel Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+    }
+}
---
+++ Source/Examples/WPF/PlotModelIssue/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PlotModelIssue
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+                MyItems.Add(new Item("MyName" + i));
+        }
+
+        private ObservableCollection<Item> myItems = new ObservableCollection<Item>();
+        public ObservableCollection<Item> MyItems
+        {
+            get { return myItems; }
+            set { myItems = value; }
+        }
+    }
+}
