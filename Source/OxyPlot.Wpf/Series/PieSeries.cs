21c106a13d62019559a2b85aca6e03e0147e2e50
Ilja Nosik <ilja.nosik@outlook.com>
2016-05-18 08:08:25 +0200

    Add XML documentation to the PieSeries class

--- Source/OxyPlot.Wpf/Series/PieSeries.cs
+++ Source/OxyPlot.Wpf/Series/PieSeries.cs
@@ -13,8 +13,14 @@ namespace OxyPlot.Wpf
     using System.Windows;
     using System.Windows.Media;
 
+    /// <summary>
+    /// This is a wrapper of OxyPlot.PieSeries.
+    /// </summary>
     public class PieSeries : ItemsSeries
     {
+        /// <summary>
+        /// Identifies the <see cref="Stroke"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty StrokeProperty =
             DependencyProperty.Register(
                 "Stroke",
@@ -22,6 +28,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(Colors.White, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="StrokeThickness"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty StrokeThicknessProperty =
             DependencyProperty.Register(
                 "StrokeThickness",
@@ -29,6 +38,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(1.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="Diameter"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty DiameterProperty =
             DependencyProperty.Register(
                 "Diameter",
@@ -36,6 +48,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(1.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="InnerDiameter"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty InnerDiameterProperty =
             DependencyProperty.Register(
                 "InnerDiameter",
@@ -43,6 +58,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(0.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="StartAngle"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty StartAngleProperty =
             DependencyProperty.Register(
                 "StartAngle",
@@ -50,6 +68,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(0.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="AngleSpan"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty AngleSpanProperty =
             DependencyProperty.Register(
                 "AngleSpan",
@@ -57,6 +78,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(360.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="AngleIncrement"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty AngleIncrementProperty =
             DependencyProperty.Register(
                 "AngleIncrement",
@@ -64,6 +88,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(1.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="LegendFormat"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty LegendFormatProperty =
             DependencyProperty.Register(
                 "LegendFormat",
@@ -71,6 +98,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(null, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="OutsideLabelFormat"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty OutsideLabelFormatProperty =
             DependencyProperty.Register(
                 "OutsideLabelFormat",
@@ -78,6 +108,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata("{2:0} %", AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="InsideLabelColor"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty InsideLabelColorProperty =
             DependencyProperty.Register(
                 "InsideLabelColor",
@@ -85,6 +118,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(MoreColors.Automatic, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="InsideLabelFormat"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty InsideLabelFormatProperty =
             DependencyProperty.Register(
                 "InsideLabelFormat",
@@ -92,6 +128,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata("{1}", AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="InsideLabelPosition"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty InsideLabelPositionProperty =
             DependencyProperty.Register(
                 "InsideLabelPosition",
@@ -99,6 +138,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(0.5, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="AreInsideLabelsAngled"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty AreInsideLabelsAngledProperty =
             DependencyProperty.Register(
                 "AreInsideLabelsAngled",
@@ -106,6 +148,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(false, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="TickDistance"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty TickDistanceProperty =
             DependencyProperty.Register(
                 "TickDistance",
@@ -113,6 +158,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(0.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="TickRadialLength"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty TickRadialLengthProperty =
             DependencyProperty.Register(
                 "TickRadialLength",
@@ -120,6 +168,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(6.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="TickHorizontalLength"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty TickHorizontalLengthProperty =
             DependencyProperty.Register(
                 "TickHorizontalLength",
@@ -127,6 +178,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(8.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="TickLabelDistance"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty TickLabelDistanceProperty =
             DependencyProperty.Register(
                 "TickLabelDistance",
@@ -134,6 +188,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(4.0, AppearanceChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="ExplodedDistance"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty ExplodedDistanceProperty =
             DependencyProperty.Register(
                 "ExplodedDistance",
@@ -141,6 +198,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(0.0));
 
+        /// <summary>
+        /// Identifies the <see cref="LabelField"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty LabelFieldProperty =
             DependencyProperty.Register(
                 "LabelField",
@@ -148,6 +208,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(null, DataChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="ValueField"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty ValueFieldProperty =
             DependencyProperty.Register(
                 "ValueField",
@@ -155,6 +218,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(null, DataChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="ColorField"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty ColorFieldProperty =
             DependencyProperty.Register(
                 "ColorField",
@@ -162,6 +228,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(null, DataChanged));
 
+        /// <summary>
+        /// Identifies the <see cref="IsExplodedField"/> dependency property.
+        /// </summary>
         public static readonly DependencyProperty IsExplodedFieldProperty =
             DependencyProperty.Register(
                 "IsExplodedField",
@@ -169,6 +238,9 @@ namespace OxyPlot.Wpf
                 typeof(PieSeries),
                 new PropertyMetadata(null, DataChanged));
 
+        /// <summary>
+        /// Initializes static members of the <see cref="PieSeries"/> class.
+        /// </summary>
         static PieSeries()
         {
             TrackerFormatStringProperty
@@ -177,149 +249,227 @@ namespace OxyPlot.Wpf
                     new PropertyMetadata(OxyPlot.Series.PieSeries.DefaultTrackerFormatString, AppearanceChanged));
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PieSeries"/> class.
+        /// </summary>
         public PieSeries()
         {
             InternalSeries = new OxyPlot.Series.PieSeries();
         }
 
+        /// <summary>
+        /// Creates the underlying model.
+        /// </summary>
+        /// <returns>A series.</returns>
         public override OxyPlot.Series.Series CreateModel()
         {
             SynchronizeProperties(InternalSeries);
             return InternalSeries;
         }
 
+        /// <summary>
+        /// Gets or sets the stroke color.
+        /// </summary>
         public Color Stroke
         {
             get { return (Color)GetValue(StrokeProperty); }
             set { SetValue(StrokeProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the stroke thickness.
+        /// </summary>
         public double StrokeThickness
         {
             get { return (double)GetValue(StrokeThicknessProperty); }
             set { SetValue(StrokeThicknessProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the diameter.
+        /// </summary>
         public double Diameter
         {
             get { return (double)GetValue(DiameterProperty); }
             set { SetValue(DiameterProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the inner diameter.
+        /// </summary>
         public double InnerDiameter
         {
             get { return (double)GetValue(InnerDiameterProperty); }
             set { SetValue(InnerDiameterProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the start angle.
+        /// </summary>
         public double StartAngle
         {
             get { return (double)GetValue(StartAngleProperty); }
             set { SetValue(StartAngleProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the angle span.
+        /// </summary>
         public double AngleSpan
         {
             get { return (double)GetValue(AngleSpanProperty); }
             set { SetValue(AngleSpanProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the angle increment.
+        /// </summary>
         public double AngleIncrement
         {
             get { return (double)GetValue(AngleIncrementProperty); }
             set { SetValue(AngleIncrementProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the legend format.
+        /// </summary>
         public string LegendFormat
         {
             get { return (string)GetValue(LegendFormatProperty); }
             set { SetValue(LegendFormatProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the outside label format.
+        /// </summary>
         public string OutsideLabelFormat
         {
             get { return (string)GetValue(OutsideLabelFormatProperty); }
             set { SetValue(OutsideLabelFormatProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the color of the inside labels.
+        /// </summary>
         public Color InsideLabelColor
         {
             get { return (Color)GetValue(InsideLabelColorProperty); }
             set { SetValue(InsideLabelColorProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the inside label format.
+        /// </summary>
         public string InsideLabelFormat
         {
             get { return (string)GetValue(InsideLabelFormatProperty); }
             set { SetValue(InsideLabelFormatProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the inside label position.
+        /// </summary>
         public double InsideLabelPosition
         {
             get { return (double)GetValue(InsideLabelPositionProperty); }
             set { SetValue(InsideLabelPositionProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether inside labels are angled.
+        /// </summary>
         public bool AreInsideLabelsAngled
         {
             get { return (bool)GetValue(AreInsideLabelsAngledProperty); }
             set { SetValue(AreInsideLabelsAngledProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the distance from the edge of the pie slice to the tick line.
+        /// </summary>
         public double TickDistance
         {
             get { return (double)GetValue(TickDistanceProperty); }
             set { SetValue(TickDistanceProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the length of the radial part of the tick line.
+        /// </summary>
         public double TickRadialLength
         {
             get { return (double)GetValue(TickRadialLengthProperty); }
             set { SetValue(TickRadialLengthProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the length of the horizontal part of the tick.
+        /// </summary>
         public double TickHorizontalLength
         {
             get { return (double)GetValue(TickHorizontalLengthProperty); }
             set { SetValue(TickHorizontalLengthProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the distance from the tick line to the outside label.
+        /// </summary>
         public double TickLabelDistance
         {
             get { return (double)GetValue(TickLabelDistanceProperty); }
             set { SetValue(TickLabelDistanceProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the exploded distance.
+        /// </summary>
         public double ExplodedDistance
         {
             get { return (double)GetValue(ExplodedDistanceProperty); }
             set { SetValue(ExplodedDistanceProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the property containing the label.
+        /// </summary>
         public string LabelField
         {
             get { return (string)GetValue(LabelFieldProperty); }
             set { SetValue(LabelFieldProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the property containing the value.
+        /// </summary>
         public string ValueField
         {
             get { return (string)GetValue(ValueFieldProperty); }
             set { SetValue(ValueFieldProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the property containing the color.
+        /// </summary>
         public string ColorField
         {
             get { return (string)GetValue(ColorFieldProperty); }
             set { SetValue(ColorFieldProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the property indicating whether the item
+        /// is exploded.
+        /// </summary>
         public string IsExplodedField
         {
             get { return (string)GetValue(IsExplodedFieldProperty); }
             set { SetValue(IsExplodedFieldProperty, value); }
         }
 
+        /// <summary>
+        /// Synchronizes the properties of this object with the underlying series object.
+        /// </summary>
+        /// <param name="series">The series.</param>
         protected override void SynchronizeProperties(OxyPlot.Series.Series series)
         {
             base.SynchronizeProperties(series);
