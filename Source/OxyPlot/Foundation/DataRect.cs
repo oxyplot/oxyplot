68feb3211ee400aada4f4b1638f7cbdf521178ca
Matt Williams <matthew.williams@saludamedical.com>
2016-09-06 13:26:02 +1000

    Added classes DataRect and DataRectSeries. These are similar to DataPoint and DataPoint series, but for series with rectangular elements, such as the HeatMapSeries. Note that the reflection data binding has not been implemented yet.

---
+++ Source/OxyPlot/Foundation/DataRect.cs
@@ -0,0 +1,132 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataRect.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// <summary>
+//   Represents a rectangle in the data space.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OxyPlot
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Represents a point in the data space.
+    /// </summary>
+    /// <remarks><see cref="DataRect" />s are transformed to <see cref="ScreenRect" />s.</remarks>
+    public struct DataRect : ICodeGenerating, IEquatable<DataRect>
+    {
+        /// <summary>
+        /// The undefined.
+        /// </summary>
+        public static readonly DataRect Undefined = new DataRect(DataPoint.Undefined, DataPoint.Undefined);
+
+        /// <summary>
+        /// The x-coordinate.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:AccessibleFieldsMustBeginWithUpperCaseLetter",
+            Justification = "Reviewed. Suppression is OK here.")]
+        internal readonly DataPoint a;
+
+        /// <summary>
+        /// The y-coordinate.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:AccessibleFieldsMustBeginWithUpperCaseLetter",
+            Justification = "Reviewed. Suppression is OK here.")]
+        internal readonly DataPoint b;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataRect" /> struct.
+        /// </summary>
+        /// <param name="x1">The x coordinate of the first corner.</param>
+        /// <param name="x2">The x coordinate of the diagonally-opposite corner.</param>
+        /// <param name="y1">The y coordinate of the first corner.</param>
+        /// <param name="y2">The y coordinate of the diagonally-opposite corner.</param>
+        public DataRect(double x1, double x2, double y1, double y2)
+        {
+            this.a = new DataPoint(x1, y1);
+            this.b = new DataPoint(x2, y2);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataRect" /> struct.
+        /// </summary>
+        /// <param name="a">The first corner.</param>
+        /// <param name="b">The diagonally-opposite corner.</param>
+        public DataRect(DataPoint a, DataPoint b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        /// <summary>
+        /// Gets the first data point.
+        /// </summary>
+        /// <value>The first data point.</value>
+        public DataPoint A
+        {
+            get
+            {
+                return this.a;
+            }
+        }
+
+        /// <summary>
+        /// Gets the diagonally-opposite data point.
+        /// </summary>
+        /// <value>The diagonally-opposite data point.</value>
+        public DataPoint B
+        {
+            get
+            {
+                return this.b;
+            }
+        }
+
+        /// <summary>
+        /// Returns C# code that generates this instance.
+        /// </summary>
+        /// <returns>The to code.</returns>
+        public string ToCode()
+        {
+            return CodeGenerator.FormatConstructor(this.GetType(), "{0},{1}", this.a, this.b);
+        }
+
+        /// <summary>
+        /// Determines whether this instance and another specified <see cref="T:DataRect" /> object have the same value.
+        /// </summary>
+        /// <param name="other">The point to compare to this instance.</param>
+        /// <returns><c>true</c> if the value of the <paramref name="other" /> parameter is the same as the value of this instance; otherwise, <c>false</c>.</returns>
+        public bool Equals(DataRect other)
+        {
+            return this.a.Equals(other.a) && this.b.Equals(other.b);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return this.a + " " + this.b;
+        }
+
+        /// <summary>
+        /// Determines whether this point is defined.
+        /// </summary>
+        /// <returns><c>true</c> if this point is defined; otherwise, <c>false</c>.</returns>
+        public bool IsDefined()
+        {
+            // check that x and y is not NaN (the code below is faster than double.IsNaN)
+#pragma warning disable 1718
+            // ReSharper disable EqualExpressionComparison
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            return this.a.IsDefined() && this.b.IsDefined();
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+            // ReSharper restore EqualExpressionComparison
+#pragma warning restore 1718
+        }
+    }
+}
---
+++ Source/OxyPlot/Series/DataRectSeries.cs
@@ -0,0 +1,205 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataRectSeries.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// <summary>
+//   Provides an abstract base class for series that contain a collection of <see cref="DataRect" />s.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OxyPlot.Series
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Provides an abstract base class for series that contain a collection of <see cref="DataPoint" />s.
+    /// </summary>
+    public abstract class DataRectSeries : XYAxisSeries
+    {
+        /// <summary>
+        /// The list of data points.
+        /// </summary>
+        private readonly List<DataRect> rects = new List<DataRect>();
+
+        /// <summary>
+        /// The data points from the items source.
+        /// </summary>
+        private List<DataRect> itemsSourceRects;
+
+        /// <summary>
+        /// Specifies if the itemsSourcePoints list can be modified.
+        /// </summary>
+        private bool ownsItemsSourcePoints;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the tracker can interpolate points.
+        /// </summary>
+        public bool CanTrackerInterpolatePoints { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delegate used to map from <see cref="ItemsSeries.ItemsSource" /> to the <see cref="ActualRects" />. The default is <c>null</c>.
+        /// </summary>
+        /// <value>The mapping.</value>
+        /// <remarks>Example: series1.Mapping = item => new DataRect(new DataPoint((MyType)item).Time1,((MyType)item).Value1), new DataPoint((MyType)item).Time2,((MyType)item).Value2));</remarks>
+        public Func<object, DataRect> Mapping { get; set; }
+
+        /// <summary>
+        /// Gets the list of points.
+        /// </summary>
+        /// <value>A list of <see cref="DataPoint" />.</value>
+        public List<DataRect> Rects
+        {
+            get
+            {
+                return this.rects;
+            }
+        }
+
+        /// <summary>
+        /// Gets the list of points that should be rendered.
+        /// </summary>
+        /// <value>A list of <see cref="DataPoint" />.</value>
+        protected List<DataRect> ActualRects
+        {
+            get
+            {
+                return this.ItemsSource != null ? this.itemsSourceRects : this.rects;
+            }
+        }
+
+        ///// <summary>
+        ///// Gets the point on the series that is nearest the specified point.
+        ///// </summary>
+        ///// <param name="point">The point.</param>
+        ///// <param name="interpolate">Interpolate the series if this flag is set to <c>true</c>.</param>
+        ///// <returns>A TrackerHitResult for the current hit.</returns>
+        //public override TrackerHitResult GetNearestPoint(ScreenPoint point, bool interpolate)
+        //{
+        //    if (interpolate && !this.CanTrackerInterpolatePoints)
+        //    {
+        //        return null;
+        //    }
+
+        //    TrackerHitResult result = null;
+        //    if (interpolate)
+        //    {
+        //        result = this.GetNearestInterpolatedPointInternal(this.ActualRects, point);
+        //    }
+
+        //    if (result == null)
+        //    {
+        //        result = this.GetNearestPointInternal(this.ActualRects, point);
+        //    }
+
+        //    if (result != null)
+        //    {
+        //        result.Text = StringHelper.Format(
+        //            this.ActualCulture,
+        //            this.TrackerFormatString,
+        //            result.Item,
+        //            this.Title,
+        //            this.XAxis.Title ?? XYAxisSeries.DefaultXAxisTitle,
+        //            this.XAxis.GetValue(result.DataPoint.X),
+        //            this.YAxis.Title ?? XYAxisSeries.DefaultYAxisTitle,
+        //            this.YAxis.GetValue(result.DataPoint.Y));
+        //    }
+
+        //    return result;
+        //}
+
+        /// <summary>
+        /// Updates the data.
+        /// </summary>
+        protected internal override void UpdateData()
+        {
+            if (this.ItemsSource == null)
+            {
+                return;
+            }
+
+            this.UpdateItemsSourcePoints();
+        }
+
+        /// <summary>
+        /// Updates the maximum and minimum values of the series.
+        /// </summary>
+        protected internal override void UpdateMaxMin()
+        {
+            base.UpdateMaxMin();
+            List<DataPoint> allDataPoints = new List<DataPoint>();
+            allDataPoints.AddRange(this.ActualRects.Select(rect => rect.A));
+            allDataPoints.AddRange(this.ActualRects.Select(rect => rect.B));
+            this.InternalUpdateMaxMin(allDataPoints);
+        }
+
+        /// <summary>
+        /// Gets the item at the specified index.
+        /// </summary>
+        /// <param name="i">The index of the item.</param>
+        /// <returns>The item of the index.</returns>
+        protected override object GetItem(int i)
+        {
+            var actualPoints = this.ActualRects;
+            if (this.ItemsSource == null && actualPoints != null && i < actualPoints.Count)
+            {
+                return actualPoints[i];
+            }
+
+            return base.GetItem(i);
+        }
+
+        /// <summary>
+        /// Clears or creates the <see cref="itemsSourceRects"/> list.
+        /// </summary>
+        private void ClearItemsSourcePoints()
+        {
+            if (!this.ownsItemsSourcePoints || this.itemsSourceRects == null)
+            {
+                this.itemsSourceRects = new List<DataRect>();
+            }
+            else
+            {
+                this.itemsSourceRects.Clear();
+            }
+
+            this.ownsItemsSourcePoints = true;
+        }
+
+        /// <summary>
+        /// Updates the points from the <see cref="ItemsSeries.ItemsSource" />.
+        /// </summary>
+        private void UpdateItemsSourcePoints()
+        {
+            // Use the Mapping property to generate the points
+            if (this.Mapping != null)
+            {
+                this.ClearItemsSourcePoints();
+                foreach (var item in this.ItemsSource)
+                {
+                    this.itemsSourceRects.Add(this.Mapping(item));
+                }
+
+                return;
+            }
+
+            var sourceAsListOfDataRects = this.ItemsSource as List<DataRect>;
+            if (sourceAsListOfDataRects != null)
+            {
+                this.itemsSourceRects = sourceAsListOfDataRects;
+                this.ownsItemsSourcePoints = false;
+                return;
+            }
+
+            this.ClearItemsSourcePoints();
+
+            var sourceAsEnumerableDataRects = this.ItemsSource as IEnumerable<DataRect>;
+            if (sourceAsEnumerableDataRects != null)
+            {
+                this.itemsSourceRects.AddRange(sourceAsEnumerableDataRects);
+                return;
+            }
+        }
+    }
+}
