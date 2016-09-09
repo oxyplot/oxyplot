e69f9e6c00f2b2f32af4a69bb9cf5c4c2bee6391
Matt Williams <matthew.williams@saludamedical.com>
2016-09-09 16:44:46 +1000

    Added window to examples for testing.

---
+++ Source/Examples/WPF/WpfExamples/Examples/HeatMapMappingDemo/HeatMapMappingWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace HeatMapMappingDemo
+{
+    using System.Windows;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    using WpfExamples;
+
+    /// <summary>
+    /// Interaction logic for HeatMapMappingWindow.xaml
+    /// </summary>
+    [Example("Demonstrates the HeatMapSeries with mapped ItemsSource.")]
+    public partial class HeatMapMappingWindow : Window
+    {
+        public PlotModel PlotModel { get; set; }
+
+        public HeatMapMappingWindow()
+        {
+            this.PlotModel = new PlotModel();
+
+            this.PlotModel.Series.Add(new HeatMapSeries()
+                                          {
+
+                                          });
+
+            this.InitializeComponent();
+        }
+    }
+}
