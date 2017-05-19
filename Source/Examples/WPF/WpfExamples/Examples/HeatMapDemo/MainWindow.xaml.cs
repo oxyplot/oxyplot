6f5d5ba732b29826c59c6e11efd012cd5ed3a8e6
Sarah Müller <sy-mueller@gmx-topmail.de>
2017-05-19 07:25:33 +0200

    1087: Added InvalidNumberColor to Wpf.LinearColorAxis and added an example for binding to InvalidNumberColor (#1089)
    removed xml documentationFiles

--- Source/Examples/WPF/WpfExamples/Examples/HeatMapDemo/MainWindow.xaml.cs
+++ Source/Examples/WPF/WpfExamples/Examples/HeatMapDemo/MainWindow.xaml.cs
@@ -10,7 +10,7 @@
 namespace HeatMapDemo
 {
     using System;
-
+    using System.Reflection;
     using WpfExamples;
 
     /// <summary>
@@ -27,6 +27,8 @@ namespace HeatMapDemo
             this.InitializeComponent();
             this.DataContext = this;
             this.Data = this.GenerateHeatMap();
+
+            this.DataNaNValues = this.GenerateHeatMapNaNValues();
         }
 
         /// <summary>
@@ -55,5 +57,33 @@ namespace HeatMapDemo
 
             return result;
         }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        public double[,] DataNaNValues { get; private set; }
+
+        /// <summary>
+        /// Generates the heat map data.
+        /// </summary>
+        /// <returns>
+        /// The heat map data array.
+        /// </returns>
+        private double[,] GenerateHeatMapNaNValues()
+        {
+            const int Rows = 100;
+            const int Cols = 100;
+            var result = new double[Rows, Cols];
+            for (var i = 0; i < Rows; i++)
+            {
+                for (var j = 0; j < Cols; j++)
+                {
+                    result[i, j] = (i==j)?double.NaN :Math.Sin(2 * Math.PI * i / Rows) * Math.Sin(2 * Math.PI * j / Cols);
+                }
+            }
+
+            return result;
+        }
+
     }
 }
