0265f5ec4d070617d32d9054fc05b9631a8abac0
RUF <rusamentiaga@uniovi.es>
2019-06-20 22:38:06 +0200

    Example for three colors in histogram series: normal distribution with 95% confidence interval for the mean

--- Source/Examples/ExampleLibrary/Series/HistogramSeriesExamples.cs
+++ Source/Examples/ExampleLibrary/Series/HistogramSeriesExamples.cs
@@ -37,6 +37,12 @@ namespace ExampleLibrary
             return CreateDisconnectedBins();
         }
 
+        [Example("Normal Distribution Three Colors")]
+        public static PlotModel NormalDistribution()
+        {
+            return CreateNormalDistribution();
+        }
+
         public static PlotModel CreateExponentialDistribution(double mean = 1, int n = 10000)
         {
             var model = new PlotModel { Title = "Exponential Distribution", Subtitle = "Uniformly distributed bins (" + n + " samples)" };
@@ -71,6 +77,28 @@ namespace ExampleLibrary
             return model;
         }
 
+        public static PlotModel CreateNormalDistribution(double mean = 0, double std = 1, int n = 1000000)
+        {
+            var model = new PlotModel { Title = "Normal Distribution (μ=0, σ=1)", Subtitle = "95% of the distribution (" + n + " samples)" };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Frequency" });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "x" });
+
+            Random rnd = new Random();
+
+            HistogramSeries chs = new HistogramSeries();
+            chs.Items.AddRange(HistogramHelpers.Collect(SampleNormal(rnd, mean, std, n), -4, 4, 100, true));
+            chs.StrokeThickness = 1;
+
+            chs.LimitHi = mean + 1.96 * std;
+            chs.LimitLo = mean - 1.96 * std;
+            chs.ColorHi = OxyColors.DarkRed;
+            chs.ColorLo = OxyColors.DarkRed;
+
+            model.Series.Add(chs);
+
+            return model;
+        }
+
         public static PlotModel CreateDisconnectedBins()
         {
             var model = new PlotModel { Title = "Disconnected Bins" };
@@ -99,6 +127,22 @@ namespace ExampleLibrary
         private static double SampleExp(Random rnd, double mean)
         {
             return Math.Log(1.0 - rnd.NextDouble()) / -mean;
+        }
+
+        private static IEnumerable<double> SampleNormal(Random rnd, double mean, double std, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return SampleNormal(rnd, mean, std);
+            }
+        }
+
+        private static double SampleNormal(Random rnd, double mean, double std)
+        {
+            // http://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+            var u1 = rnd.NextDouble();
+            var u2 = rnd.NextDouble();
+            return Math.Sqrt(-2 * Math.Log(u1)) * Math.Cos(2 * Math.PI * u2);
         }
     }
 }
