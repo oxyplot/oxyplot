5f9d84645313870ec3397a04be7169f90e2283b0
Silviu Bogan <silviubogan@gmail.com>
2019-10-14 13:28:44 +0300

    Started working on IToolTip and its WinForms-based implementation

---
+++ Source/OxyPlot/Utilities/IToolTip.cs
@@ -0,0 +1,14 @@
+﻿namespace OxyPlot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IToolTip
+    {
+        void Show();
+        void Hide();
+        string Text { get; set; }
+        void Dispose();
+    }
+}
