c78b8ca451c741481d5ea231b42a6fecc1411506
Oystein Bjorke <oystein.bjorke@gmail.com>
2019-10-27 10:34:24 +0100

    Move WPF Plot component to Oxyplot.Contrib.Wpf (#1399)

---
+++ Source/OxyPlot.Contrib.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="OxyPlot">
+//   Copyright (c) 2014 OxyPlot contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: CLSCompliant(false)]
+
+[assembly: XmlnsPrefix("http://oxyplot.org/wpf/contrib", "oxyc")]
+[assembly: XmlnsDefinition("http://oxyplot.org/wpf/contrib", "OxyPlot.Wpf")]
