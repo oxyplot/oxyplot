ce91a6eb7eb91fe0ae6d097f90901ead439865ec
Jonathan Arweck <jonathan.arweck@gmail.com>
2020-10-24 16:22:22 +0200

    add open contour example

--- Source/Examples/ExampleLibrary/Series/ContourSeriesExamples.cs
+++ Source/Examples/ExampleLibrary/Series/ContourSeriesExamples.cs
@@ -20,6 +20,10 @@ namespace ExampleLibrary
                - 10 * (x / 5 - x * x * x - y * y * y * y * y) * Math.Exp(-x * x - y * y)
                - 1.0 / 3 * Math.Exp(-(x + 1) * (x + 1) - y * y);
 
+        private static Func<double, double, double> openContours = (x, y) =>
+            (x * x) / (y * y + 1)
+          + (y * y) / (x * x + 1);
+
         [Example("Peaks")]
         public static PlotModel Peaks()
         {
@@ -165,6 +169,21 @@ namespace ExampleLibrary
             };
             cs.Data = ArrayBuilder.Evaluate(peaks, cs.ColumnCoordinates, cs.RowCoordinates);
             model.Subtitle = cs.Data.GetLength(0) + "×" + cs.Data.GetLength(1);
+            model.Series.Add(cs);
+            return model;
+        }
+
+        [Example("Open Contours")]
+        public static PlotModel OpenContours()
+        {
+            var model = new PlotModel();
+            var cs = new ContourSeries
+            {
+                ColumnCoordinates = ArrayBuilder.CreateVector(-3, 3, 0.05),
+                RowCoordinates = ArrayBuilder.CreateVector(-3, 3, 0.05)
+            };
+
+            cs.Data = ArrayBuilder.Evaluate(openContours, cs.ColumnCoordinates, cs.RowCoordinates);
             model.Series.Add(cs);
             return model;
         }
