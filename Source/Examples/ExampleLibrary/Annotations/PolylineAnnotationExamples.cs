cdac4031a220c4c834f8934177875b64a6144155
Murdo R Ergeaux <visual.melon@gmail.com>
2022-02-19 12:38:18 +0000

    Add fractal examples for PolygonAnnotation and PolylineAnnotations (#1853)

--- Source/Examples/ExampleLibrary/Annotations/PolylineAnnotationExamples.cs
+++ Source/Examples/ExampleLibrary/Annotations/PolylineAnnotationExamples.cs
@@ -31,5 +31,36 @@ namespace ExampleLibrary
             model.Annotations.Add(a2);
             return model;
         }
+
+        [Example("Koch Surfaces")]
+        public static PlotModel KochSurface()
+        {
+            DataPoint[] plane(DataPoint centre)
+            {
+                return new[]
+                {
+                    new DataPoint(centre.X - 1, centre.Y),
+                    new DataPoint(centre.X + 1, centre.Y),
+                };
+            }
+
+            var model = new PlotModel { Title = "PolygonAnnotation", PlotType = PlotType.Cartesian };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = -2, Maximum = 2 });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Minimum = -2, Maximum = 2 });
+
+            var a1 = new PolylineAnnotation { Text = "MSL = 4", MinimumSegmentLength = 4, LineStyle = LineStyle.Solid, TextPosition = new DataPoint(0, 1) };
+            a1.Points.AddRange(PolygonAnnotationExamples.KochFractal(plane(new DataPoint(0, 1)), 8, true, false));
+            model.Annotations.Add(a1);
+
+            var a2 = new PolylineAnnotation { Text = "MSL = 2", MinimumSegmentLength = 2, LineStyle = LineStyle.Solid, TextPosition = new DataPoint(0, 0) };
+            a2.Points.AddRange(PolygonAnnotationExamples.KochFractal(plane(new DataPoint(0, 0)), 8, true, false));
+            model.Annotations.Add(a2);
+
+            var a3 = new PolylineAnnotation { Text = "MSL = 1", MinimumSegmentLength = 1, LineStyle = LineStyle.Solid, TextPosition = new DataPoint(0, -1) };
+            a3.Points.AddRange(PolygonAnnotationExamples.KochFractal(plane(new DataPoint(0, -1)), 8, true, false));
+            model.Annotations.Add(a3);
+
+            return model;
+        }
     }
 }
