fb619c106dd64270e48493b7abad1e9f72ea16ff
Carl Reinke <carlreinke@users.noreply.github.com>
2022-04-29 15:33:20 -0600

    Add plot manipulator axis preference

---
+++ Source/OxyPlot/PlotController/Manipulators/AxisPreference.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AxisPreference.cs" company="OxyPlot">
+//   Copyright (c) 2022 OxyPlot contributors
+// </copyright>
+// <summary>
+//   Specifies which axis a manipulator will prefer to operate on.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OxyPlot
+{
+    /// <summary>
+    /// Specifies which axis a manipulator will prefer to operate on.
+    /// </summary>
+    public enum AxisPreference
+    {
+        /// <summary>
+        /// Manipulation will not prefer a particular axis.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Manipulation will prefer to operate on the X axis.
+        /// </summary>
+        X,
+
+        /// <summary>
+        /// Manipulation will prefer to operate on the Y axis.
+        /// </summary>
+        Y,
+    }
+}
